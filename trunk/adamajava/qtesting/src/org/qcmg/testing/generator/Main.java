90b0e2101a9f27e2e805054b4ad18da01fddaf47
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2014-04-08 02:49:37 +0000

    adding qtesting

---
+++ trunk/adamajava/qtesting/src/org/qcmg/testing/generator/Main.java
@@ -0,0 +1,9 @@
+/**
+ * © Copyright The University of Queensland 2010-2014.  This code is released under the terms outlined in the included LICENSE file.
+ */
+package org.qcmg.testing.generator;
+
+public final class Main {
+	public static void main(final String[] args) {
+	}
+}
