50aa461a388a97cc3edaf7686d418b4d760e4ae0
adama test <christina.xu@qimrberghofer.edu.au>
2014-04-24 03:43:51 +0000

    upload 0.2per testing

---
+++ qcnv/test/org/qcmg/cnv/MtCountsTest.java
@@ -0,0 +1,47 @@
+package org.qcmg.cnv;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.*;
+
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMRecord;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.qcmg.common.log.QLogger;
+import org.qcmg.picard.SAMFileReaderFactory;
+import org.qcmg.picard.SAMOrBAMWriterFactory;
+
+public class MtCountsTest {
+	
+	public static final String[] INPUTs = { "input.normal.bam","input.tumor.bam"};
+	 
+
+	@After
+	public void deleteIO(){
+		for (String f : INPUTs)
+			new File(f).delete();
+	 
+		
+	}
+	@Before
+	public void createIO(){
+		TestFiles.CreateBAM(INPUTs[0], INPUTs[1]);
+	}
+	
+	@Test
+	public void main() throws Exception{
+		String output = "output.counts";
+		String[] ids = {"tumor" ,"normal"};
+		String[] args = {"-i", INPUTs[0],"-i", INPUTs[1], "--id", ids[0] , "--id", ids[1], "-o" , output};
+		Main.main(args);
+		
+	}
+
+}
