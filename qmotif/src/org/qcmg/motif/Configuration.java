8f281c900760bcc5dc2eb7275d3cc191fab54b21
holmeso <holmeso@users.noreply.github.com>
2014-07-07 01:35:03 +0000

    removed unnecessary code

--- qmotif/src/org/qcmg/motif/Configuration.java
+++ qmotif/src/org/qcmg/motif/Configuration.java
@@ -4,16 +4,11 @@
 package org.qcmg.motif;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Vector;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.ini4j.Ini;
-import org.qcmg.common.log.QLogger;
-import org.qcmg.common.log.QLoggerFactory;
 import org.qcmg.common.model.ChrPosition;
 import org.qcmg.common.util.Pair;
 import org.qcmg.motif.util.IniUtils;
@@ -22,31 +17,18 @@ import org.qcmg.qbamfilter.query.QueryExecutor;
 
 public final class Configuration {
 	private final int numberThreads;
-	private final String outputFileName;
-	private final String inputGFF3FileName;
-	private final String[] bamFileNames;
-	private String[] baiFileNames;
-	private final File inputGFF3File;
 	private final File outputFile;
-	private File[] inputBAMFiles;
-	private File[] inputBAIFiles;
 	private final HashSet<Pair<File, File>> filePairs = new HashSet<Pair<File, File>>();
-	private final Options options;
 	private final QueryExecutor filter;
 	private final Algorithm algorithm;
 	private final LoggerInfo loggerInfo;
 	private final String validation;
 	private final AtomicLong countReadFromInput;
 	private final AtomicLong countReadToCoverage;
-	private Motifs stageOneMotifs;
-	private final String stageOneRegex;
-	private Motifs stageTwoMotifs;
-	private final String stageTwoRegex;
 	private final List<ChrPosition> excludes;
 	private final List<ChrPosition> includes;
 	private final Integer windowSize;
 	private final Integer cutoff;
-	private final String outputGffFileName;
 	private final String outputBamFileName;
 	private final  Ini iniFile;
 	private final MotifsAndRegexes mAndR;
@@ -58,18 +40,17 @@ public final class Configuration {
 		return includes;
 	}
 
-	private final QLogger logger;
-
 	public Configuration(final Options options) throws Exception {
 		options.detectBadOptions();
-		this.options = options;
 		
 		iniFile = new Ini(new File(options.getIniFile()));
 			
 		windowSize = Integer.parseInt(IniUtils.getEntry(iniFile, "PARAMS", "window_size"));
 		
-		stageOneRegex = IniUtils.getEntry(iniFile, "PARAMS", "stage1_motif_regex");
-		stageTwoRegex = IniUtils.getEntry(iniFile, "PARAMS", "stage2_motif_regex");
+		String stageOneRegex = IniUtils.getEntry(iniFile, "PARAMS", "stage1_motif_regex");
+		String stageTwoRegex = IniUtils.getEntry(iniFile, "PARAMS", "stage2_motif_regex");
+		Motifs stageOneMotifs = null;
+		Motifs stageTwoMotifs = null;
 		
 		String s1Motifs = IniUtils.getEntry(iniFile, "PARAMS", "stage1_motif_string");
 		if (null != s1Motifs) {
@@ -88,17 +69,13 @@ public final class Configuration {
 		excludes = IniUtils.getPositions(iniFile, "EXCLUDES");
 			
 		loggerInfo = new LoggerInfo(options);
-		logger = QLoggerFactory.getLogger(Configuration.class);
-		
 		validation = options.getValidation();
 
-		outputFileName = options.getOutputFileNames()[0];
-		outputGffFileName = options.getOutputFileNames().length > 1 ? options.getOutputFileNames()[1] : null;
-		outputBamFileName = options.getOutputFileNames().length > 2 ? options.getOutputFileNames()[2] : null;
-		inputGFF3FileName = options.getInputGFF3FileNames()[0];
-		bamFileNames = options.getBAMFileNames();
-		baiFileNames = options.getBAIFileNames();
-		inferMissingBaiFileNames();
+		outputBamFileName = options.getOutputFileNames().length > 1 ? options.getOutputFileNames()[1] : null;
+		String [] bamFileNames = options.getBAMFileNames();
+		String [] baiFileNames = options.getBAIFileNames();
+		
+		inferFilePairings(bamFileNames, baiFileNames);
 
 		if (options.hasNumberThreadsOption()) {
 			numberThreads = options.getNumberThreads().intValue();
@@ -112,28 +89,12 @@ public final class Configuration {
 			filter = null;
 		}
 		
-		inputGFF3File = new File(inputGFF3FileName);
-		instantiateBamFiles();
-		instantiateBaiFiles();
-		outputFile = new File(outputFileName);
+		outputFile = new File(options.getOutputFileNames()[0]);
 		checkFileExistence();
-		inferFilePairings();
 		countReadFromInput = new AtomicLong();
 		countReadToCoverage = new AtomicLong();
 		
 		cutoff = Integer.parseInt(IniUtils.getEntry(iniFile, "PARAMS", "cutoff_size"));
-	}
-	
-	Map<ChrPosition, String> getMapFromList(List<ChrPosition> cps) {
-		Map<ChrPosition, String> map = new HashMap<>();
-		for (ChrPosition cp : cps) {
-			map.put(cp, null);
-		}
-		return map;
-	}
-	
-	public Options getOptions() {
-		return options;
 	}
 	
 	public AtomicLong getInputReadsCount(){
@@ -148,33 +109,6 @@ public final class Configuration {
 		return numberThreads;
 	}
 
-	public String getOutputFileName() {
-		return outputFileName;
-	}
-
-	public String getInputGFF3FileName() {
-		return inputGFF3FileName;
-	}
-
-	public File getInputGFF3File() {
-		return inputGFF3File;
-	}
-
-	public String[] getBamFileNames() {
-		return bamFileNames;
-	}
-
-	public String[] getBaiFileNames() {
-		return baiFileNames;
-	}
-
-	public File[] getInputBAMFiles() {
-		return inputBAMFiles;
-	}
-
-	public File[] getInputBAIFiles() {
-		return inputBAIFiles;
-	}
 
 	public File getOutputFile() {
 		return outputFile;
@@ -192,70 +126,33 @@ public final class Configuration {
 		return algorithm;
 	}
 
-	private void inferMissingBaiFileNames() throws Exception {
-		if (null == baiFileNames) {
-			inferAllBaiFileNames();
-		} else if (0 == baiFileNames.length) {
-			inferAllBaiFileNames();
-		} else if (baiFileNames.length < bamFileNames.length) {
-			throw new Exception(
-					"Partial BAI file name lists not yet supported. Provide either none or all BAI file names.");
-		} else if (baiFileNames.length > bamFileNames.length) {
-			throw new Exception(
-					"Too many BAI files specified. Provide either none or a number of BAI file names equal to the number of BAM files.");
-		} else {
-			// No inferencing necessary: equal number of BAI and BAM file names.
-		}
-	}
-
-	private void inferAllBaiFileNames() {
-		Vector<String> baiFileNameList = new Vector<String>();
-		for (String bamFileName : bamFileNames) {
-			baiFileNameList.add(bamFileName + ".bai");
-		}
-		baiFileNames = new String[baiFileNameList.size()];
-		baiFileNameList.toArray(baiFileNames);
-	}
-
-	private void instantiateBaiFiles() {
-		inputBAIFiles = new File[baiFileNames.length];
-		int i = 0;
-		for (String baiFileName : baiFileNames) {
-			inputBAIFiles[i] = new File(baiFileName);
-			i++;
-		}
-	}
-
-	private void instantiateBamFiles() {
-		inputBAMFiles = new File[bamFileNames.length];
-		int i = 0;
-		for (String bamFileName : bamFileNames) {
-			inputBAMFiles[i] = new File(bamFileName);
-			i++;
-		}
-	}
-
-	private void inferFilePairings() {
-		assert (inputBAMFiles.length == inputBAIFiles.length);
+	private void inferFilePairings(String [] inputBAMFiles, String [] inputBAIFiles) {
 		for (int i = 0; i < inputBAMFiles.length; i++) {
-			File bamFile = inputBAMFiles[i];
-			File baiFile = inputBAIFiles[i];
-			Pair<File, File> filePair = new Pair<>(bamFile, baiFile);
+			String bamFile = inputBAMFiles[i];
+			String baiFile = null;
+			if (null != inputBAIFiles && inputBAIFiles.length > 0) {
+				baiFile = inputBAIFiles[i];
+			}
+			Pair<File, File> filePair = new Pair<>(new File(bamFile), null == baiFile ? null : new File(baiFile));
 			filePairs.add(filePair);
 		}
 	}
 
 	private void checkFileExistence() throws Exception {
-		for (final File file : inputBAMFiles) {
-			if (!file.exists()) {
-				throw new Exception("Input BAM file '" + file.getAbsolutePath()
-						+ "' does not exist");
+		// loop through filePairs
+		
+		for (Pair<File, File> entry : filePairs) {
+			if (null != entry.getLeft()) {
+				if ( ! entry.getLeft().exists()) {
+					throw new Exception("Input BAM file '" + entry.getLeft().getAbsolutePath()
+							+ "' does not exist");
+				}
 			}
-		}
-		for (final File file : inputBAIFiles) {
-			if (!file.exists()) {
-				throw new Exception("Input BAI file '" + file.getAbsolutePath()
-						+ "' does not exist");
+			if (null != entry.getRight()) {
+				if ( ! entry.getRight().exists()) {
+					throw new Exception("Input BAI file '" + entry.getRight().getAbsolutePath()
+							+ "' does not exist");
+				}
 			}
 		}
 	}
@@ -276,9 +173,6 @@ public final class Configuration {
 		return cutoff;
 	}
 
-	public String getOutputGff() {
-		return outputGffFileName;
-	}
 	public String getOutputBam() {
 		return outputBamFileName;
 	}
