56b9112f561359b142743ae3073c60027c69f698
adama test <christina.xu@qimrberghofer.edu.au>
2014-08-08 03:34:37 +0000

    add draft code

---
+++ qmito/src/au/edu/qimr/qmito/Messages.java
@@ -0,0 +1,66 @@
+/**
+ * © Copyright The University of Queensland 2010-2014.  This code is released under the terms outlined in the included LICENSE file.
+ */
+package au.edu.qimr.qmito;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+/*
+ * a collection of methods which return formated message string
+ */
+public class Messages {
+    static final ResourceBundle messages =
+        ResourceBundle.getBundle("au.edu.qimr.qmito.messages");
+
+    public static String USAGE = getMessage("USAGE");
+
+    public static String getMessage(final String identifier)
+    {
+        return messages.getString(identifier);
+    }
+
+    static String getMessage(final String identifier,
+                             final String argument)
+    {
+         String message = Messages.getMessage(identifier);
+        Object[] arguments = {argument};
+        return MessageFormat.format(message, arguments);
+    }
+
+    static String getMessage(final String identifier,
+                             final String arg1,
+                             final String arg2)
+    {
+        final String message = Messages.getMessage(identifier);
+        Object[] arguments = {arg1, arg2};
+        return MessageFormat.format(message, arguments);
+    }
+    static String getMessage(final String identifier,
+                             final String arg1,
+                             final String arg2,
+                             final String arg3)
+    {
+        final String message = Messages.getMessage(identifier);
+        Object[] arguments = {arg1, arg2, arg3};
+        return MessageFormat.format(message, arguments);
+    }
+
+    static String getMessage(final String identifier,
+                             final Object[] arguments)
+    {
+        final String message = Messages.getMessage(identifier);
+        return MessageFormat.format(message, arguments);
+    }
+
+    /**
+	 * Gets the program name.
+	 *
+	 * @return the program name
+	 */
+	static String getProgramName() {
+		return Messages.class.getPackage().getImplementationTitle();
+	}
+
+
+
+}
