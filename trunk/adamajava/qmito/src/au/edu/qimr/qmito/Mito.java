145722c27202a89e457f131fe0da6c1851000f14
qxu-sourceforge <qxu-sourceforge@8d477179-88b4-446a-adef-fc416ee005a4>
2014-08-08 03:33:57 +0000

    add draft code

--- trunk/adamajava/qmito/src/au/edu/qimr/qmito/Mito.java
+++ trunk/adamajava/qmito/src/au/edu/qimr/qmito/Mito.java
@@ -1,11 +1,46 @@
 package au.edu.qimr.qmito;
 
+import org.qcmg.common.log.QLogger;
+import org.qcmg.common.log.QLoggerFactory;
 import org.qcmg.common.util.LoadReferencedClasses;
+import org.qcmg.pileup.QPileup;
+
 
 
 public class Mito {
+	
+	private static QLogger logger;
+	
+	
 	public static void main(String[] args) throws Exception {
-		 
+		
+	       try {
+	            Options options = new Options(args);
+	            logger = QLoggerFactory.getLogger(Mito.class, options.getLogFileName(), options.getLogLevel());
+	            
+	            if ( options.hasCommandChecked()){    
+					LoadReferencedClasses.loadClasses(QPileup.class);
+          	
+	               logger.logInitialExecutionStats(options.getPGName(), options.getVersion(),args);
+	               logger.tool("query: " + options.getQuery());
+	               logger.tool("input: " +options.getInputFileName());
+	               logger.tool("output: " +options.getOutputFileName());	
+	               logger.info("logger level " + options.getLogLevel());	               
+	               
+	//               QueryMT multiQuery = new QueryMT(options, logger);
+              
+	               logger.logFinalExecutionStats(0);
+	            }
+	        }catch (Exception e) {
+	        	e.printStackTrace();
+	        	System.err.println(Thread.currentThread().getName() + " " + e.toString());
+	        if (null != logger) {
+	        	logger.info(Thread.currentThread().getName() + " " + e.toString());	            
+	            logger.logFinalExecutionStats(1);
+	        	}
+	            System.exit(1);
+	        }	
+
 	}	
 
 }
