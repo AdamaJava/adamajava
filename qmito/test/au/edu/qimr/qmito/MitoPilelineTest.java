dec83fe88674ee91f95547d8b3556781768744a9
adama test <christina.xu@qimrberghofer.edu.au>
2014-08-26 06:33:00 +0000

    Add unit test for last three base only with three reads

---
+++ qmito/test/au/edu/qimr/qmito/MitoPilelineTest.java
@@ -0,0 +1,113 @@
+package au.edu.qimr.qmito;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+import net.sf.picard.reference.IndexedFastaSequenceFile;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.qcmg.picard.SAMFileReaderFactory;
+import org.qcmg.qbamfilter.query.QueryExecutor;
+
+import au.edu.qimr.qlib.qpileup.PositionElement;
+import au.edu.qimr.qlib.qpileup.QPileupRecord;
+import au.edu.qimr.qlib.qpileup.StrandDS;
+import au.edu.qimr.qlib.qpileup.StrandElement;
+import au.edu.qimr.qlib.qpileup.StrandEnum;
+import au.edu.qimr.qlib.util.Reference;
+import net.sf.picard.reference.FastaSequenceIndex;
+import net.sf.picard.reference.IndexedFastaSequenceFile;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
+import net.sf.samtools.SAMRecordIterator;
+import net.sf.samtools.SAMFileReader.ValidationStringency;
+import net.sf.samtools.SAMSequenceRecord;
+
+public class MitoPilelineTest {
+	private String input = "./input.bam";
+	private String reference = "./reference.fa";
+	private String log = "./output.log";
+	private String output = "./output.tsv";
+ 
+	
+	@Before
+	public void createInput(){	
+		TestFile.CreateBam(input);
+	}
+	@After
+	public void deleteInput(){	
+		new File(input + ".bai").delete();
+		new File(input).delete();
+	}
+	
+	@Test
+	public void pilelineTest(){
+		//here we use fake reference file, since it won't be used only passing to option 
+		String[] args = {"-r", input,"-i", input, "-o", output, "--log",log, "--lowreadcount","2", "--nonrefthreshold" , "50" };
+		try {
+			Options options = new Options(args);
+			//here we can't call mito.report since we testing data don't provide reference index file		
+			MitoPileline mito = new MitoPileline(options);
+		    
+			//Here we only test last three base of chrMT
+			int posStart = 16567;			
+			int[][] positionCounts = new int[][]{
+					//position 16567: 1 read mapped with referece base 'A' ; total 1 reads < 2 lowreadcount is ture; nonreference Reads is 0 < 50%; so highNonreference is false
+					{1,0,0,0,0,1,0},  
+					//position 16568: 2 reads mapped with referece base 'T'; total 2 reads >= 2  lowreadcount is false; nonreference Reads is 0, ighNonreference is false
+					{0,2,0,0,0,0,0}, 
+					//position 16569: 1 read mapped with referece base 'G', 2 with non referenece base 'C'; 
+					//total reads 3 >= 2 lowreadcount is false;  nonreference Reads is 2 > 50%, ighNonreference is true
+					{0,0,1,2,1,0,2}    
+			};
+				
+			Map<String, StrandElement> map;	 
+			
+			for (int i = 0; i < 3; i ++){
+				int[] counts = new int[7];
+				 
+				 map  = mito.GetReverseStrandDS().getStrandElementMap(posStart + i - 1);
+				 counts[0] = (int) map.get(StrandEnum.baseA.toString()).getStrandElementMember(0);
+				 counts[1] = (int) map.get(StrandEnum.baseT.toString()).getStrandElementMember(0);
+				 counts[2] = (int) map.get(StrandEnum.baseG.toString()).getStrandElementMember(0);
+				 counts[3] = (int) map.get(StrandEnum.baseC.toString()).getStrandElementMember(0);
+				 counts[4] = (int) map.get(StrandEnum.highNonreference.toString()).getStrandElementMember(0);
+				 counts[5] = (int) map.get(StrandEnum.lowRead.toString()).getStrandElementMember(0);
+				 counts[6] = (int) map.get(StrandEnum.nonreferenceNo.toString()).getStrandElementMember(0);
+ 				 Assert.assertTrue(Arrays.equals(positionCounts[i], counts));				 	 
+			}
+				 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+ 	private void readReads(Options options ){
+		SAMSequenceRecord ref =  options.getReferenceRecord();
+	 
+		SAMFileReader reader = SAMFileReaderFactory.createSAMFileReader(input,ValidationStringency.SILENT);
+		SAMRecordIterator ite = reader.query(ref.getSequenceName(),0, ref.getSequenceLength(), false);
+		while(ite.hasNext()){
+			SAMRecord record = ite.next();	
+			String Sbase = new String(record.getReadBases());
+			 
+			System.out.println(String.format("(%d~%d:) %s",  record.getAlignmentStart() , record.getAlignmentEnd(), Sbase));
+			int start = record.getAlignmentEnd() - 3;
+			int end = record.getAlignmentEnd(); 
+		//	System.out.println(String.format("base (%d:) %s", start ,  new String(record.getReadBases()).substring(Sbase.length()-4)));
+		}
+		reader.close();
+	}
+}
---
+++ qmito/test/au/edu/qimr/qmito/TestFile.java
@@ -0,0 +1,73 @@
+package au.edu.qimr.qmito;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMRecord;
+
+import org.junit.Test;
+import org.qcmg.picard.SAMFileReaderFactory;
+import org.qcmg.picard.SAMOrBAMWriterFactory;
+
+import net.sf.picard.reference.FastaSequenceIndex;
+import net.sf.picard.reference.IndexedFastaSequenceFile;
+
+public class TestFile {
+	 
+    public static void CreateFile(List<String> data, String fileName){
+
+       BufferedWriter out;
+       try {
+          out = new BufferedWriter(new FileWriter(fileName));
+          for (String line : data) {
+                  out.write(line + "\n");
+          }
+          out.close();
+       } catch (IOException e) {
+           System.err.println("IOException caught whilst attempting to write to SAM test file: "
+                                              + fileName + e);
+       }
+ }
+
+		public static void CreateBam(String output){
+			String sam = "./input.sam";
+			CreateFile(TestFile.CreateSam(), sam);
+			SAMFileReader reader = SAMFileReaderFactory.createSAMFileReader(sam,  SAMFileReader.ValidationStringency.SILENT);  
+			SAMOrBAMWriterFactory factory = new SAMOrBAMWriterFactory(reader.getFileHeader(), true, new File(output),true );									 
+			SAMFileWriter writer = factory.getWriter();
+	       
+	    	for( SAMRecord record : reader)
+	    			writer.addAlignment(record);	
+	    
+	    	factory.closeWriter();
+	    	reader.close();
+	    	new File(sam).delete();
+			
+		}
+		
+		/**
+		 * There are three reversed mapped and primary reads, two of them mapped on edge. These testing data are modified from real SAM records.
+		 * @return a list of text SAMRecord 
+		 */
+	   public static List<String> CreateSam(){
+	        List<String> data = new ArrayList<String>();
+	        data.add("@HD	VN:1.4	GO:none	SO:coordinate");
+	        data.add("@SQ	SN:chrMT	LN:16569");	        	       	        
+	        data.add("@RG	ID:20130123010211418	PL:IONTORRENT	PU:PGM/316D/IonXpress_001	LB:ICGC_HG19/IonXpress_001	FO:TACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACGTCTGAGCATCGATCGATGTACAGCTACGTACG	zc:5:/mnt/seq_results/smgres_special/COLO_829/seq_mapped/T00002_20120206_140.nopd.IonXpress_001.bam	DS:Manual ePCR	DT:2012-12-30T02:33:23+1000	PG:tmap	SM:COLO_pool_29gene_Manual	KS:TCAGCTAAGGTAACGAT	CN:TorrentServer/T02");
+	        data.add("@PG	ID:0bea213c-c8ff-489d-b4f4-5ce5b0ddb5f3	PN:qbammerge	zc:8	VN:0.6pre (6608)	CL:qbammerge --output /scratch/432375.qcmg-clustermk2.imb.uq.edu.au/SmgresSpecial_COLO829_1DNA_9CellLineDerivedFromTumour_ICGCMSSM2011051602CD_IonTorrentMultiplexedLibraryBuilder_29GeneCancerPanelTargetSEQ_Tmap_IonPGM.jpearson.Library_20120201_O_Library_20120202_A.bam --log /scratch/432375.qcmg-clustermk2.imb.uq.edu.au/SmgresSpecial_COLO829_1DNA_9CellLineDerivedFromTumour_ICGCMSSM2011051602CD_IonTorrentMultiplexedLibraryBuilder_29GeneCancerPanelTargetSEQ_Tmap_IonPGM.jpearson.Library_20120201_O_Library_20120202_A.bam.qmrg.log --input /mnt/seq_results/smgres_special/COLO_829/seq_mapped/T00002_20120206_140.nopd.IonXpress_001.bam --input /mnt/seq_results/smgres_special/COLO_829/seq_mapped/T00002_20120128_135.nopd.IonXpress_001.bam --input /mnt/seq_results/smgres_special/COLO_829/seq_mapped/T00002_20120308_149.nopd.IonXpress_001.bam");	        	        
+	        data.add("HWI-ST1240:115:H03J8ADXX:2:1216:5509:54570	83	chrMT	16479	29	91M10S	=	16251	-319	TAAAGTGAACTGTATCCGACATCTGGTTCCTACTTCAGGGCCATAAAGCCTAAATAGCCCACACGTTCCCCTTAAATAAGACATCACGATGGATCACAGGT	4A:CCDC@>CCCBBBB?>5@C=CCEEEEEHHEECED@@GGDIGHIEGIGFACGHGGIIIHFDDFEGIIIIHFAEHHHHEC<FG>E:AGAHHFHDDDDD@@;	ZC:i:8	MD:Z:40T50	PG:Z:MarkDuplicates	RG:Z:20121125035550829	XG:i:0	AM:i:29	NM:i:1	SM:i:29	XM:i:1	XO:i:0	XT:A:M");
+	        data.add("HWI-ST1240:137:H08VDADXX:1:2105:6130:72695	177	chrMT	16568	37	101M	=	138	-16430	TCCATCGTGATGTCTTATTTAAGGGGAACGTGTGGGCTATTTAGGCTTTATGGCCCTGAAGTAGGAACCAGATGTCGGATACAGTTCACTTTAGCTACCCC	#####BBBBB<BBBBBBBB<<0<0<70'FFFFFFF<BBFFBBFFFFIIIFIFFBBBFFFFB7FFFBFFB7FF<BFIFBFFI<BFB<F<BBBFBB<BFF<BB	X0:i:1	X1:i:0	ZC:i:10	MD:Z:1G0A1A0A0A0A0A1A0A0A0A0A0A1A0A0A2A0A0A0A2A0A0A0A0A0A0A0A0A0A1A0A0A1A0A0A0A0A0A1A0A0A0A0A0A0A0A2A0A1A0A2A0A1A1A0A0A0A0A0A1A1A1A0A0A0A1A0A0A0A1A0A0A1A0A0A0A0	PG:Z:MarkDuplicates	RG:Z:20130123010211418	XG:i:0	AM:i:37	NM:i:77	SM:i:37	XM:i:2	XO:i:0	XT:A:U");
+	        data.add("HWI-ST1240:137:H08VDADXX:1:1206:5690:25092	177	chrMT	16569	37	101M	=	39	-16530	CCATCGTGATGTCTTATTTAAGGGTAACGTGTGGGCTATTTAGGCTTTATGGCCCTGAAGTAGGAACCAGATGTCGGATACAGTTCACTTTAGCTACCCCC	BBBBB<0<BBBBB<BBBBBBBBBBBBBBBBFBBFFFBFFFFBB0<BFFFBBBFB7FBFBFBFFB0BF<<0IFBFFBBBFFFF7IFFBFBB<FBB07FFBBB	X0:i:1	X1:i:0	ZC:i:10	MD:Z:0G0A1A0A0A0A0A1A0A0A0A0A0A1A0A0A2A0A0A0A2A0A0A0A0A0A0A0A0A0A1A0A0A1A0A0A0A0A0A1A0A0A0A0A0A0A0A2A0A1A0A2A0A1A1A0A0A0A0A0A1A1A1A0A0A0A1A0A0A0A1A0A0A1A0A0A0A0A0	PG:Z:MarkDuplicates	RG:Z:20130123010211418	XG:i:0	AM:i:37	NM:i:78	SM:i:37	XM:i:3	XO:i:0	XT:A:U");
+
+	        //data.add("HWI-ST1240:137:H08VDADXX:1:2105:6130:72695	181	chrMT	16568	37	101M	=	138	-16430	TCCATCGTGATGTCTTATTTAAGGGGAACGTGTGGGCTATTTAGGCTTTATGGCCCTGAAGTAGGAACCAGATGTCGGATACAGTTCACTTTAGCTACCCC	#####BBBBB<BBBBBBBB<<0<0<70'FFFFFFF<BBFFBBFFFFIIIFIFFBBBFFFFB7FFFBFFB7FF<BFIFBFFI<BFB<F<BBBFBB<BFF<BB	X0:i:1	X1:i:0	ZC:i:10	MD:Z:1G0A1A0A0A0A0A1A0A0A0A0A0A1A0A0A2A0A0A0A2A0A0A0A0A0A0A0A0A0A1A0A0A1A0A0A0A0A0A1A0A0A0A0A0A0A0A2A0A1A0A2A0A1A1A0A0A0A0A0A1A1A1A0A0A0A1A0A0A0A1A0A0A1A0A0A0A0	PG:Z:MarkDuplicates	RG:Z:20130123010211418	XG:i:0	AM:i:37	NM:i:77	SM:i:37	XM:i:2	XO:i:0	XT:A:U");
+	        
+	        return data;
+	    }
+	
+}
