a74b5ba082973c789aafe0d54872ff5bf6109f3f
qxu-sourceforge <qxu-sourceforge@8d477179-88b4-446a-adef-fc416ee005a4>
2014-08-27 02:20:53 +0000

    modify build files

--- trunk/adamajava/qmito/src/au/edu/qimr/qmito/Mito.java
+++ trunk/adamajava/qmito/src/au/edu/qimr/qmito/Mito.java
@@ -9,36 +9,36 @@ public class Mito {
 	
 		
 	public static void main(String[] args) throws Exception {		
-	       try {
-	            Options options = new Options(args);
-	            logger = QLoggerFactory.getLogger(Mito.class, options.getLogFileName(), options.getLogLevel());
-	            
-	            if ( options.hasCommandChecked()){    
-					LoadReferencedClasses.loadClasses(Mito.class);          	
-	               logger.logInitialExecutionStats(options.getPGName(), options.getVersion(),args);
-		       	   for (String bamFile: options.getInputFileNames()) 
-		       			logger.info("input Bam: "  + bamFile);
-	               
-	               logger.tool("output: " +options.getOutputFileName());
-	               logger.tool("query: " + options.getQuery());	
-	               logger.tool("reference File: " + options.getReferenceFile());
-	               logger.tool("reference record name: " + options.getReferenceRecord().getSequenceName());
-	               logger.tool("Low Read Count: " + options.getLowReadCount());
-	               logger.tool("NonReference Threshold: " + options.getNonRefThreshold());
-	               logger.info("logger level " + options.getLogLevel());	
-	               new MitoPileline(options).report();
-		          
-		           logger.logFinalExecutionStats(0);
-	            }
-	        }catch (Exception e) {
-	        	e.printStackTrace();
-	        	System.err.println(Thread.currentThread().getName() + " " + e.toString());
-		        if (null != logger) {
-		        	logger.info(Thread.currentThread().getName() + " " + e.toString());	            
-		            logger.logFinalExecutionStats(1);
-		        }
-		        System.exit(1);	     	
-	       }
+		LoadReferencedClasses.loadClasses(Mito.class);    
+       try {
+            Options options = new Options(args);
+            logger = QLoggerFactory.getLogger(Mito.class, options.getLogFileName(), options.getLogLevel());
+            
+            if ( options.hasCommandChecked()){    					     	
+               logger.logInitialExecutionStats(options.getPGName(), options.getVersion(),args);
+	       	   for (String bamFile: options.getInputFileNames()) 
+	       			logger.info("input Bam: "  + bamFile);
+               
+               logger.tool("output: " +options.getOutputFileName());
+               logger.tool("query: " + options.getQuery());	
+               logger.tool("reference File: " + options.getReferenceFile());
+               logger.tool("reference record name: " + options.getReferenceRecord().getSequenceName());
+               logger.tool("Low Read Count: " + options.getLowReadCount());
+               logger.tool("NonReference Threshold: " + options.getNonRefThreshold());
+               logger.info("logger level " + options.getLogLevel());	
+               new MitoPileline(options).report();
+	          
+	           logger.logFinalExecutionStats(0);
+            }
+        }catch (Exception e) {
+        	e.printStackTrace();
+        	System.err.println(Thread.currentThread().getName() + " " + e.toString());
+	        if (null != logger) {
+	        	logger.info(Thread.currentThread().getName() + " " + e.toString());	            
+	            logger.logFinalExecutionStats(1);
+	        }
+	        System.exit(1);	     	
+       }
 	     
 	}	
 
