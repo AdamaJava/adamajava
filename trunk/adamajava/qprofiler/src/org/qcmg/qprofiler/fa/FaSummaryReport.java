9c42ec06a902f62ac5cb5bf192ffaf0379aa6506
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2014-12-16 00:38:41 +0000

    removed kmer by position tallying for fa analysis

--- trunk/adamajava/qprofiler/src/org/qcmg/qprofiler/fa/FaSummaryReport.java
+++ trunk/adamajava/qprofiler/src/org/qcmg/qprofiler/fa/FaSummaryReport.java
@@ -9,9 +9,7 @@
  */
 package org.qcmg.qprofiler.fa;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
@@ -26,18 +24,8 @@ public class FaSummaryReport extends SummaryReport {
 	ConcurrentMap<String, AtomicLong> kmers = new ConcurrentHashMap<>();
 	
 	
-	List<ConcurrentMap<String, AtomicLong>> kmerArray =  new ArrayList<ConcurrentMap<String, AtomicLong>>();
-	
-	final int kmerArraySize = 256;
-	
-	
 	public FaSummaryReport() {
 		super();
-		
-		//populate kmerArray with empty maps
-		for (int i = 0 ; i < kmerArraySize ; i++) {
-			kmerArray.add(new ConcurrentHashMap<String, AtomicLong>());
-		}
 	}
 	
 	@Override
@@ -46,14 +34,6 @@ public class FaSummaryReport extends SummaryReport {
 		Element element = init(parent, ProfileType.FA, null, null, null);
 		logger.info("no of kmers: " + kmers.size());
 		SummaryReportUtils.lengthMapToXml(element, "KMERS", kmers);
-		
-		
-		// print some stats on the kmerArray
-		for (int i = 0 ; i < kmerArraySize ; i++) {
-			// we want position, and number of kmers at that position
-			ConcurrentMap<String, AtomicLong> map = kmerArray.get(i);
-			logger.info("position: " + i + ", no of kmers at this pos: " + map.size());
-		}
 	}
 	
 	/**
@@ -71,7 +51,6 @@ public class FaSummaryReport extends SummaryReport {
 			for (int i = 0, len = record.length - kmerLength ; i < len ; i++) {
 				String kmer = new String(Arrays.copyOfRange(record, i, i+kmerLength));
 				updateMap(kmers, kmer);
-				updateMapAndPosition(kmerArray, kmer, i);
 			}
 		}
 	}
@@ -84,26 +63,6 @@ public class FaSummaryReport extends SummaryReport {
 				if (null != existing) {
 					al = existing;
 				}
-		}
-		al.incrementAndGet();
-	}
-	
-	private <T> void updateMapAndPosition(List<ConcurrentMap<T, AtomicLong>> collection , T key, int position) {
-		
-		if (position > kmerArraySize) {
-			throw new IllegalArgumentException("position is larger than size of array! position: " + position + ", array size: " + kmerArraySize);
-		}
-		// get map at this position
-		ConcurrentMap<T, AtomicLong> map = collection.get(position);
-		
-		
-		AtomicLong al = map.get(key);
-		if (null == al) {
-			al = new AtomicLong();
-			AtomicLong existing = map.putIfAbsent(key, al);
-			if (null != existing) {
-				al = existing;
-			}
 		}
 		al.incrementAndGet();
 	}
