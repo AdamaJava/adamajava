e231a1e80e16da9b5af6883e23ca0f21554291c0
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2015-01-06 00:43:08 +0000

    some updates to the vcf header classes and the was SignatureGenerator uses them

--- trunk/adamajava/qcommon/src/org/qcmg/common/vcf/header/VcfHeaderFilter.java
+++ trunk/adamajava/qcommon/src/org/qcmg/common/vcf/header/VcfHeaderFilter.java
@@ -14,28 +14,25 @@ import org.qcmg.common.util.Constants;
 public final class  VcfHeaderFilter extends VcfHeaderRecord{
  
 
-	public VcfHeaderFilter(String line) throws Exception {
+	public VcfHeaderFilter(String line) {
 		super(line);
 		// Is this an Info line?
 		if(this.type.equals(MetaType.FILTER)) 
 			 parseLine(line);
-		else throw new RuntimeException("Line provided is not an FILTER definition: '" + line + "'");
+		else throw new IllegalArgumentException("Can't create VcfHeaderFilter - line provided is not an FILTER definition: '" + line + "'");
 		
 		record = this;
-		
 	}
-
 	
 	public VcfHeaderFilter(String id, String description) throws Exception{ 
 		 super(null );
 		 this.id = id;
 		 this.description = description;
 		 type = MetaType.FILTER;
-		 line = type.toString() +  "=<ID=" + id//
+		 line = type.toString() +  "<ID=" + id//
 	 				+ ",Description=\"" + description + "\"" + ">" ;
 		 
 		 record = this;
-		
 	}
 	
 	@Override
