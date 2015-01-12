d588fa4d43743326420354c0f389504df5338c03
adama test <christina.xu@qimrberghofer.edu.au>
2015-01-12 05:53:49 +0000

    fix bugs on foramt column ';' to ':' bwt attributes; make attributes unique

--- qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
+++ qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
@@ -1,7 +1,7 @@
 package org.qcmg.common.vcf;
 
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.qcmg.common.util.Constants;
@@ -10,16 +10,18 @@ public class VcfFormatFieldRecord {
 //	static final String NULL = "NULL";
 	
 	//add . if missing value;
-	final Map<String,String> field = new HashMap<>();	
+	final Map<String,String> field = new LinkedHashMap<>();	
  
 //	final String sampleId; //eg. Control, Test
-	String sample = null;
+//	String sample = null;
+	
+ 
 	
 	/**
-	 * parse line into <key, value> pairs. eg. "CONF;NNS=5" will be parsed to <CONF,NULL>, <NNS,5>
-	 * @param line
+	 * 
+	 * @param format  Format column string. eg. "GT:GD:AC"
+	 * @param sample  one of the Sample column String. eg.  "0/1:C/A:A0[0],15[36.2],C11[36.82],9[33]"
 	 */
-	//public VcfFormatFieldRecord(String format, String sample, String id){
 	public VcfFormatFieldRecord(String format, String sample){	
 		final String[] keys = format.split(Constants.COLON_STRING);	
 		
@@ -27,38 +29,59 @@ public class VcfFormatFieldRecord {
 			field.put(keys[i], Constants.MISSING_DATA_STRING);
 		
 //		sampleId = id;
-		this.sample = sample;
-		if(sample == null) return;
+//		this.sample = sample;
+//		if(sample == null) return;
 	
 		final String[] values= sample.split(Constants.COLON_STRING);
 		for(int i = 0; i < field.size(); i ++) 
-			if(values.length > i && values[i] != null)
+			if(values.length > i ) // && values[i] != null)
 				field.put(keys[i], values[i] );
-		 
+			else
+				field.put(keys[i], Constants.MISSING_DATA+"");
 	}
 	
 
-	
-	
 	public String getfield(String key){
-		return field.get(key);
+		try{
+			return field.get(key);
+		}catch(final NullPointerException e){
+			return null;
+		}
 	}
 	
 	
-	@Override
-	public String toString(){
-		if(sample != null)
-			return sample;
-		
-		sample = "";
+	/**
+	 * 
+	 * @return Format column String. eg.  GT:GQ:DP:HQ
+	 */
+	public String getFormatColumnString(){
+		String str = "";
 		final Iterator<String> it = field.keySet().iterator();
 		while(it.hasNext()){
 			final String key = it.next();
-			sample += (sample != "")? Constants.SEMI_COLON_STRING : "";
+			str += (str != "")? Constants.COLON_STRING : "";
+			str += key;
+		}
+		
+		return (str == "")? null: str;
+	}
+	
+
+	/**
+	 * @return return sample column String followed Format column pattern: eg.  0|0:48:1:51,51
+	 */
+	@Override
+	public String toString(){
+		
+		String sample = "";
+		final Iterator<String> it = field.keySet().iterator();
+		while(it.hasNext()){
+			final String key = it.next();
+			sample += (sample != "")? Constants.COLON_STRING : "";
 			sample += field.get(key);
 		}
 		
-		return sample;
+		return (sample == "")? null: sample;
 	}
 
  
