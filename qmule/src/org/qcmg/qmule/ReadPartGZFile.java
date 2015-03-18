9159f81e829732a5b9e780108c850193389e1e58
adama test <christina.xu@qimrberghofer.edu.au>
2015-03-18 04:29:23 +0000

    make the tool count whole gz file lines

--- qmule/src/org/qcmg/qmule/ReadPartGZFile.java
+++ qmule/src/org/qcmg/qmule/ReadPartGZFile.java
@@ -6,6 +6,7 @@ package org.qcmg.qmule;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +22,7 @@ import java.util.Set;
 import java.util.zip.GZIPInputStream;
 import java.lang.Math;
 
+import org.qcmg.common.log.QLogger;
 import org.qcmg.common.util.FileUtils;
 import org.qcmg.vcf.VCFSerializer;
 
@@ -31,39 +33,63 @@ import net.sf.samtools.SAMRecord;
 
 public class ReadPartGZFile {
 	
+	static InputStream getInputStream(File input_gzip_file) throws FileNotFoundException, IOException{
+		InputStream inputStream;		
+	       if (FileUtils.isFileGZip(input_gzip_file)) {
+	        	GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(input_gzip_file));
+	        	try(InputStreamReader streamReader = new InputStreamReader(gzis)){
+		        	 inputStream = new GZIPInputStream(new FileInputStream(input_gzip_file));
+	        	}
+	    	} else {
+		        FileInputStream stream = new FileInputStream(input_gzip_file);
+		        try(InputStreamReader streamReader = new InputStreamReader(stream)){	         
+		        	BufferedReader in = new BufferedReader(streamReader);
+		        	inputStream = new FileInputStream(input_gzip_file);
+		        }
+	    	}
+		return inputStream;
+	}
 	
 	ReadPartGZFile(File input_gzip_file, int no) throws Exception{
 		
-		InputStream inputStream;		
-       if (FileUtils.isFileGZip(input_gzip_file)) {
-        	GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(input_gzip_file));
-        	try(InputStreamReader streamReader = new InputStreamReader(gzis)){
-	        	 inputStream = new GZIPInputStream(new FileInputStream(input_gzip_file));
-        	}
-    	} else {
-	        FileInputStream stream = new FileInputStream(input_gzip_file);
-	        try(InputStreamReader streamReader = new InputStreamReader(stream)){	         
-	        	BufferedReader in = new BufferedReader(streamReader);
-	        	inputStream = new FileInputStream(input_gzip_file);
-	        }
-    	}
+	  InputStream inputStream = getInputStream(input_gzip_file);
 
-       BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream) );
-       int num = 0;
-       String line; 
-
-		while( (line = reader.readLine() ) != null){
-			if( ++num > no) break;
-			System.out.println(line);
-		} 
-
+       try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream) )){
+	       int num = 0;
+	       String line; 
+			while( (line = reader.readLine() ) != null){
+				if( ++num > no) break;
+				System.out.println(line);
+			} 
+       }
+	}
+	static void countLines(File input_gzip_file) throws FileNotFoundException, IOException{
+		InputStream inputStream = getInputStream(input_gzip_file);
+		  try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream) )){
+			  
+			  long startTime = System.currentTimeMillis();
+		      long num = 0;
+		       String line; 
+				while( (line = reader.readLine() ) != null){
+					num ++;
+				} 
+				
+			  String time = QLogger.getRunTime(startTime, System.currentTimeMillis());
+			  
+			  System.out.println(String.format("Read file: %s\nLine number: %d\nTime: %s", input_gzip_file.getAbsoluteFile(), num, time));
+	       }
 	}
 
 	public static void main(String[] args) {
 		try{
-			int no = Integer.parseInt(args[1]);
 			File input = new File(args[0]);
-			ReadPartGZFile vs = new ReadPartGZFile(input, no );	
+			int no = Integer.parseInt(args[1]);
+			
+			if(no > 0)
+				new ReadPartGZFile(input, no );	
+			else
+				countLines(input);
+			
 		}catch(Exception e){
 			e.printStackTrace();
 			//System.out.println(e.printStackTrace(););
