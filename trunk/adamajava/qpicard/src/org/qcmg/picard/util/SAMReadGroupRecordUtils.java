7400a3ae33171c202267cab92d5766ad3c0c41d0
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2015-04-15 01:20:23 +0000

    added method to recreate header lines from header object when header is too large for picard to do it

--- trunk/adamajava/qpicard/src/org/qcmg/picard/util/SAMReadGroupRecordUtils.java
+++ trunk/adamajava/qpicard/src/org/qcmg/picard/util/SAMReadGroupRecordUtils.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import net.sf.samtools.SAMReadGroupRecord;
 
 import org.qcmg.common.string.StringUtils;
+import org.qcmg.common.util.Constants;
 
 public class SAMReadGroupRecordUtils {
 	
@@ -16,32 +17,32 @@ public class SAMReadGroupRecordUtils {
 	public static String getRGString(SAMReadGroupRecord rec) {
 		if (null == rec) throw new IllegalArgumentException("null SAMReadGroupRecord passed to getRGString");
 		
-		StringBuilder sb = new StringBuilder("@RG");
+		StringBuilder sb = new StringBuilder(Constants.READ_GROUP_PREFIX);
 		if ( ! StringUtils.isNullOrEmpty(rec.getId()))
-			sb.append(StringUtils.getTabAndString("ID:")).append(rec.getId());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.READ_GROUP_ID_TAG)).append(Constants.COLON).append(rec.getId());
 		if ( ! StringUtils.isNullOrEmpty(rec.getSequencingCenter()))
-			sb.append(StringUtils.getTabAndString("CN:")).append(rec.getSequencingCenter());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.SEQUENCING_CENTER_TAG)).append(Constants.COLON).append(rec.getSequencingCenter());
 		if ( ! StringUtils.isNullOrEmpty(rec.getDescription()))
-			sb.append(StringUtils.getTabAndString("DS:")).append(rec.getDescription());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.DESCRIPTION_TAG)).append(Constants.COLON).append(rec.getDescription());
 		if (null != (rec.getRunDate()))
-			sb.append(StringUtils.getTabAndString("DT:")).append(sdf.format(rec.getRunDate()));
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.DATE_RUN_PRODUCED_TAG)).append(Constants.COLON).append(sdf.format(rec.getRunDate()));
 		if ( ! StringUtils.isNullOrEmpty(rec.getFlowOrder()))
-			sb.append(StringUtils.getTabAndString("FO:")).append(rec.getFlowOrder());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.FLOW_ORDER_TAG)).append(Constants.COLON).append(rec.getFlowOrder());
 		if ( ! StringUtils.isNullOrEmpty(rec.getKeySequence()))
-			sb.append(StringUtils.getTabAndString("KS:")).append(rec.getKeySequence());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.KEY_SEQUENCE_TAG)).append(Constants.COLON).append(rec.getKeySequence());
 		if ( ! StringUtils.isNullOrEmpty(rec.getLibrary()))
-			sb.append(StringUtils.getTabAndString("LB:")).append(rec.getLibrary());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.LIBRARY_TAG)).append(Constants.COLON).append(rec.getLibrary());
 		
 		// PG??
 		
 		if (null != rec.getPredictedMedianInsertSize())
-			sb.append(StringUtils.getTabAndString("PI:")).append(rec.getPredictedMedianInsertSize().intValue());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.PREDICTED_MEDIAN_INSERT_SIZE_TAG)).append(Constants.COLON).append(rec.getPredictedMedianInsertSize().intValue());
 		if ( ! StringUtils.isNullOrEmpty(rec.getPlatform()))
-			sb.append(StringUtils.getTabAndString("PL:")).append(rec.getPlatform());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.PLATFORM_TAG)).append(Constants.COLON).append(rec.getPlatform());
 		if ( ! StringUtils.isNullOrEmpty(rec.getPlatformUnit()))
-			sb.append(StringUtils.getTabAndString("PU:")).append(rec.getPlatformUnit());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.PLATFORM_UNIT_TAG)).append(Constants.COLON).append(rec.getPlatformUnit());
 		if ( ! StringUtils.isNullOrEmpty(rec.getSample()))
-			sb.append(StringUtils.getTabAndString("SM:")).append(rec.getSample());
+			sb.append(StringUtils.getTabAndString(SAMReadGroupRecord.READ_GROUP_SAMPLE_TAG)).append(Constants.COLON).append(rec.getSample());
 		
 		return sb.toString();
 	}
