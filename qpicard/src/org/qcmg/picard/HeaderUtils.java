225ca2c7e7ff20adba1437c35f5007360ef13f4e
holmeso <holmeso@users.noreply.github.com>
2015-04-15 01:20:23 +0000

    added method to recreate header lines from header object when header is too large for picard to do it

--- qpicard/src/org/qcmg/picard/HeaderUtils.java
+++ qpicard/src/org/qcmg/picard/HeaderUtils.java
@@ -3,11 +3,16 @@
  */
 package org.qcmg.picard;
 
-import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import net.sf.samtools.SAMFileHeader;
 import net.sf.samtools.SAMProgramRecord;
+import net.sf.samtools.SAMReadGroupRecord;
+import net.sf.samtools.SAMSequenceRecord;
+
+import org.qcmg.common.string.StringUtils;
+import org.qcmg.common.util.Constants;
+import org.qcmg.picard.util.SAMReadGroupRecordUtils;
 
 public final class HeaderUtils {
 	public static SAMProgramRecord addProgramRecord(SAMFileHeader header,
@@ -49,6 +54,75 @@ public final class HeaderUtils {
 	public static void addComment(SAMFileHeader header, String comment){
 		 
 		header.addComment(comment);		 
+	}
+	
+	
+	public static String getPGString(SAMProgramRecord rec) {
+		if (null == rec) throw new IllegalArgumentException("null SAMProgramRecord passed to getPGString");
+		
+		StringBuilder sb = new StringBuilder(Constants.PROGRAM_PREFIX);
+		if ( ! StringUtils.isNullOrEmpty(rec.getId()))
+			sb.append(StringUtils.getTabAndString(SAMProgramRecord.PROGRAM_GROUP_ID_TAG)).append(Constants.COLON).append(rec.getId());
+		if ( ! StringUtils.isNullOrEmpty(rec.getProgramName()))
+			sb.append(StringUtils.getTabAndString(SAMProgramRecord.PROGRAM_NAME_TAG)).append(Constants.COLON).append(rec.getProgramName());
+		if ( ! StringUtils.isNullOrEmpty(rec.getProgramVersion()))
+			sb.append(StringUtils.getTabAndString(SAMProgramRecord.PROGRAM_VERSION_TAG)).append(Constants.COLON).append(rec.getProgramVersion());
+		if ( ! StringUtils.isNullOrEmpty(rec.getCommandLine()))
+			sb.append(StringUtils.getTabAndString(SAMProgramRecord.COMMAND_LINE_TAG)).append(Constants.COLON).append(rec.getCommandLine());
+		if (null != (rec.getPreviousProgramGroupId()))
+			sb.append(StringUtils.getTabAndString(SAMProgramRecord.PREVIOUS_PROGRAM_GROUP_ID_TAG)).append(Constants.COLON).append(rec.getPreviousProgramGroupId());
+		
+		return sb.toString();
+	}
+	
+	
+	/**
+	 * Picard's getTextHeader returns null if there is more than 1MB of blurb in the header.
+	 * This method attempts to recreate the header text, and may truncate lines over 2k in length
+	 * 
+	 * @param header
+	 * @return
+	 */
+	public static String getHeaderStringFromHeader(SAMFileHeader header) {
+		String headerText = header.getTextHeader();
+		if (null == headerText) {
+			StringBuilder sb = new StringBuilder();
+			// header line
+			sb.append(Constants.HEADER_PREFIX).append(Constants.TAB);
+			sb.append(SAMFileHeader.VERSION_TAG).append(Constants.COLON);
+			sb.append(header.getVersion()).append(Constants.TAB);
+			sb.append(SAMFileHeader.GROUP_ORDER_TAG).append(Constants.COLON);
+			sb.append(header.getGroupOrder().toString()).append(Constants.TAB);
+			sb.append(SAMFileHeader.SORT_ORDER_TAG).append(Constants.COLON);
+			sb.append(header.getSortOrder().toString()).append(Constants.NL);
+			
+			// sequence next
+			for (SAMSequenceRecord ssr :header.getSequenceDictionary().getSequences()) {
+				sb.append(Constants.SEQUENCE_PREFIX).append(Constants.TAB);
+				sb.append(SAMSequenceRecord.SEQUENCE_NAME_TAG).append(Constants.COLON);
+				sb.append(ssr.getSequenceName()).append(Constants.TAB);
+				sb.append(SAMSequenceRecord.SEQUENCE_LENGTH_TAG).append(Constants.COLON);
+				sb.append(ssr.getSequenceLength()).append(Constants.NL);
+			}
+			
+			// readgroup next
+			for (SAMReadGroupRecord srgr : header.getReadGroups()) {
+				sb.append(SAMReadGroupRecordUtils.getRGString(srgr)).append(Constants.NL);
+			}
+			
+			// program lines now
+			for (SAMProgramRecord spr : header.getProgramRecords()) {
+				sb.append(getPGString(spr)).append(Constants.NL);
+			}
+			
+			// and finally comments
+			for (String co : header.getComments()) {
+				sb.append(Constants.COMMENT_PREFIX).append(Constants.TAB).append(co).append(Constants.NL);
+			}
+			
+			headerText = sb.toString();
+		}
+		return headerText;
 	}
 	 
 }
