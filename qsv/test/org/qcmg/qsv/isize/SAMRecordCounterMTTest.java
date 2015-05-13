5be7ee1cefe4f505ac4f45c660ff8f932ca41f32
holmeso <holmeso@users.noreply.github.com>
2015-05-13 05:54:05 +0000

    try and speed up test.....

--- qsv/test/org/qcmg/qsv/isize/SAMRecordCounterMTTest.java
+++ qsv/test/org/qcmg/qsv/isize/SAMRecordCounterMTTest.java
@@ -12,8 +12,6 @@ import java.util.List;
 
 import net.sf.samtools.SAMFileHeader.SortOrder;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -21,6 +19,8 @@ import org.qcmg.qsv.QSVException;
 import org.qcmg.qsv.annotate.RunTypeRecord;
 
 public class SAMRecordCounterMTTest {
+	
+	private static final String END_OF_READ = "\tACTCCATTTCTAGAAAAAAATTAGAAAATTAACTGGAACCAGGAGAGGTG	IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIHIIIIIII@	MD:Z:18C31	RG:Z:20110221052813657	NH:i:1	CM:i:2	NM:i:1	SM:i:97	ZP:Z:ABC	CQ:Z:BBBBBBBBABB>>7AA:@??B??;>@?>5>@@B@@?1:@?=81<::>=?@	CS:Z:T31220130022322000000303220003030121020101202222011\n";
 	
 	@Rule
 	public TemporaryFolder testFolder = new TemporaryFolder();
@@ -42,13 +42,13 @@ public class SAMRecordCounterMTTest {
 	private void createBamFile(File file) throws IOException {
 		List<String> data = new ArrayList<String>();
 		data.addAll(createSamHeader(SortOrder.unsorted));
-		data.addAll(getSAMRecords());
 
-		BufferedWriter out = new BufferedWriter(new FileWriter(file));
-        for (final String line : data) {
-            out.write(line + "" + "\n");
-        }
-        out.close();
+		try (BufferedWriter out = new BufferedWriter(new FileWriter(file));) {
+	        for (final String line : data) {
+	            out.write(line + "\n");
+	        }
+	        getSAMRecords(out);
+		}
 		
 	}
 
@@ -69,96 +69,88 @@ public class SAMRecordCounterMTTest {
 	       return data;
 	}
 
-	private Collection<? extends String> getSAMRecords() {
-		List<String> data = new ArrayList<String>();
+	private void getSAMRecords(BufferedWriter out) throws IOException {
 		
-		data.addAll(createRecords(229,"140191020\t0"));
-		data.addAll(createRecords(2,"140191020\t40"));
-		data.addAll(createRecords(2,"140191020\t80"));
-		data.addAll(createRecords(376,"140191020\t90"));
-		data.addAll(createRecords(2820,"140191020\t100"));
-		data.addAll(createRecords(5772,"140191020\t110"));
-		data.addAll(createRecords(8896,"140191020\t120"));
-		data.addAll(createRecords(11358,"140191020\t130"));
-		data.addAll(createRecords(12626,"140191020\t140"));
-		data.addAll(createRecords(13437,"140191020\t150"));
-		data.addAll(createRecords(14032,"140191020\t160"));
-		data.addAll(createRecords(14451,"140191020\t170"));
-		data.addAll(createRecords(14537,"140191020\t180"));
-		data.addAll(createRecords(14615,"140191020\t190"));
-		data.addAll(createRecords(15806,"140191020\t200"));
-		data.addAll(createRecords(16309,"140191020\t210"));
-		data.addAll(createRecords(17540,"140191020\t220"));
-		data.addAll(createRecords(20509,"140191020\t230"));
-		data.addAll(createRecords(24656,"140191020\t240"));
-		data.addAll(createRecords(28513,"140191020\t250"));
-		data.addAll(createRecords(34720,"140191020\t260"));
-		data.addAll(createRecords(40961,"140191020\t270"));
-		data.addAll(createRecords(49129,"140191020\t280"));
-		data.addAll(createRecords(58384,"140191020\t290"));
-		data.addAll(createRecords(71585,"140191020\t300"));
-		data.addAll(createRecords(73491,"140191020\t310"));
-		data.addAll(createRecords(67551,"140191020\t320"));
-		data.addAll(createRecords(57469,"140191020\t330"));
-		data.addAll(createRecords(46505,"140191020\t340"));
-		data.addAll(createRecords(35617,"140191020\t350"));
-		data.addAll(createRecords(26385,"140191020\t360"));
-		data.addAll(createRecords(17762,"140191020\t370"));
-		data.addAll(createRecords(10119,"140191020\t380"));
-		data.addAll(createRecords(4459,"140191020\t390"));
-		data.addAll(createRecords(1704,"140191020\t400"));
-		data.addAll(createRecords(782,"140191020\t410"));
-		data.addAll(createRecords(536,"140191020\t420"));
-		data.addAll(createRecords(372,"140191020\t430"));
-		data.addAll(createRecords(268,"140191020\t440"));
-		data.addAll(createRecords(266,"140191020\t450"));
-		data.addAll(createRecords(234,"140191020\t460"));
-		data.addAll(createRecords(230,"140191020\t470"));
-		data.addAll(createRecords(254,"140191020\t480"));
-		data.addAll(createRecords(214,"140191020\t490"));
-		data.addAll(createRecords(180,"140191020\t500"));
-		data.addAll(createRecords(148,"140191020\t510"));
-		data.addAll(createRecords(178,"140191020\t520"));
-		data.addAll(createRecords(64,"140191020\t530"));
-		data.addAll(createRecords(44,"140191020\t540"));
-		data.addAll(createRecords(30,"140191020\t550"));
-		data.addAll(createRecords(44,"140191020\t560"));
-		data.addAll(createRecords(20,"140191020\t570"));
-		data.addAll(createRecords(30,"140191020\t580"));
-		data.addAll(createRecords(22,"140191020\t590"));
-		data.addAll(createRecords(32,"140191020\t600"));
-		data.addAll(createRecords(28,"140191020\t610"));
-		data.addAll(createRecords(42,"140191020\t620"));
-		data.addAll(createRecords(22,"140191020\t630"));
-		data.addAll(createRecords(18,"140191020\t640"));
-		data.addAll(createRecords(10,"140191020\t650"));
-		data.addAll(createRecords(2,"140191020\t660"));
-		data.addAll(createRecords(10,"140191020\t670"));
-		data.addAll(createRecords(4,"140191020\t680"));
-		data.addAll(createRecords(8,"140191020\t690"));
-		data.addAll(createRecords(32,"140191020\t700"));
-		data.addAll(createRecords(22,"140191020\t710"));
-		data.addAll(createRecords(18,"140191020\t720"));
-		data.addAll(createRecords(8,"140191020\t730"));
-		data.addAll(createRecords(4,"140191020\t780"));
-		data.addAll(createRecords(2,"140191020\t790"));
-		data.addAll(createRecords(8,"140191020\t800"));
-		data.addAll(createRecords(2,"140191020\t810"));
-		data.addAll(createRecords(4,"140191020\t820"));
-		data.addAll(createRecords(2,"140191020\t860"));
-		return data;
+		createRecords(229,"140191020\t0", out);
+		createRecords(2,"140191020\t40", out);
+		createRecords(2,"140191020\t80", out);
+		createRecords(376,"140191020\t90", out);
+		createRecords(2820,"140191020\t100", out);
+		createRecords(5772,"140191020\t110", out);
+		createRecords(8896,"140191020\t120", out);
+		createRecords(11358,"140191020\t130", out);
+		createRecords(12626,"140191020\t140", out);
+		createRecords(13437,"140191020\t150", out);
+		createRecords(14032,"140191020\t160", out);
+		createRecords(14451,"140191020\t170", out);
+		createRecords(14537,"140191020\t180", out);
+		createRecords(14615,"140191020\t190", out);
+		createRecords(15806,"140191020\t200", out);
+		createRecords(16309,"140191020\t210", out);
+		createRecords(17540,"140191020\t220", out);
+		createRecords(20509,"140191020\t230", out);
+		createRecords(24656,"140191020\t240", out);
+		createRecords(28513,"140191020\t250", out);
+		createRecords(34720,"140191020\t260", out);
+		createRecords(40961,"140191020\t270", out);
+		createRecords(49129,"140191020\t280", out);
+		createRecords(58384,"140191020\t290", out);
+		createRecords(71585,"140191020\t300", out);
+		createRecords(73491,"140191020\t310", out);
+		createRecords(67551,"140191020\t320", out);
+		createRecords(57469,"140191020\t330", out);
+		createRecords(46505,"140191020\t340", out);
+		createRecords(35617,"140191020\t350", out);
+		createRecords(26385,"140191020\t360", out);
+		createRecords(17762,"140191020\t370", out);
+		createRecords(10119,"140191020\t380", out);
+		createRecords(4459,"140191020\t390", out);
+		createRecords(1704,"140191020\t400", out);
+		createRecords(782,"140191020\t410", out);
+		createRecords(536,"140191020\t420", out);
+		createRecords(372,"140191020\t430", out);
+		createRecords(268,"140191020\t440", out);
+		createRecords(266,"140191020\t450", out);
+		createRecords(234,"140191020\t460", out);
+		createRecords(230,"140191020\t470", out);
+		createRecords(254,"140191020\t480", out);
+		createRecords(214,"140191020\t490", out);
+		createRecords(180,"140191020\t500", out);
+		createRecords(148,"140191020\t510", out);
+		createRecords(178,"140191020\t520", out);
+		createRecords(64,"140191020\t530", out);
+		createRecords(44,"140191020\t540", out);
+		createRecords(30,"140191020\t550", out);
+		createRecords(44,"140191020\t560", out);
+		createRecords(20,"140191020\t570", out);
+		createRecords(30,"140191020\t580", out);
+		createRecords(22,"140191020\t590", out);
+		createRecords(32,"140191020\t600", out);
+		createRecords(28,"140191020\t610", out);
+		createRecords(42,"140191020\t620", out);
+		createRecords(22,"140191020\t630", out);
+		createRecords(18,"140191020\t640", out);
+		createRecords(10,"140191020\t650", out);
+		createRecords(2,"140191020\t660", out);
+		createRecords(10,"140191020\t670", out);
+		createRecords(4,"140191020\t680", out);
+		createRecords(8,"140191020\t690", out);
+		createRecords(32,"140191020\t700", out);
+		createRecords(22,"140191020\t710", out);
+		createRecords(18,"140191020\t720", out);
+		createRecords(8,"140191020\t730", out);
+		createRecords(4,"140191020\t780", out);
+		createRecords(2,"140191020\t790", out);
+		createRecords(8,"140191020\t800", out);
+		createRecords(2,"140191020\t810", out);
+		createRecords(4,"140191020\t820", out);
+		createRecords(2,"140191020\t860", out);
 	}
 
-	private Collection<? extends String> createRecords(int size, String dataString) {
-		List<String> data = new ArrayList<String>();
+	private void createRecords(int size, String dataString, BufferedWriter out) throws IOException {
+		String s = "	129	chr7	140191000	63	50M	chr7\t"+dataString+END_OF_READ;
 		for (int i=0; i<= size; i++) {
-			data.add(getSAMString(i, dataString));		
+			out.write("254_166_1407" + i + s);
 		}
-		
-		return data;
-	}
-
-	private String getSAMString(int i, String isize) {
-		return "254_166_1407"+i+"	129	chr7	140191000	63	50M	chr7\t"+isize+"\tACTCCATTTCTAGAAAAAAATTAGAAAATTAACTGGAACCAGGAGAGGTG	IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIHIIIIIII@	MD:Z:18C31	RG:Z:20110221052813657	NH:i:1	CM:i:2	NM:i:1	SM:i:97	ZP:Z:ABC	CQ:Z:BBBBBBBBABB>>7AA:@??B??;>@?>5>@@B@@?1:@?=81<::>=?@	CS:Z:T31220130022322000000303220003030121020101202222011";
 	}
 }
