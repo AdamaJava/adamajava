7a7893feb9db577eae1f4a7ad82eacd1e8d0b28f
oliverH <holmeso@users.noreply.github.com>
2015-07-06 02:08:21 +0000

    initial pass at q3tiledaligner

---
+++ src/au/edu/qimr/tiledaligner/Options.java
@@ -0,0 +1,146 @@
+/**
+ * © Copyright The University of Queensland 2010-2014.  This code is released under the terms outlined in the included LICENSE file.
+ */
+/**
+ * All source code distributed as part of the AdamaJava project is released
+ * under the GNU GENERAL PUBLIC LICENSE Version 3, a copy of which is
+ * included in this distribution as gplv3.txt.
+ */
+package au.edu.qimr.tiledaligner;
+
+import java.util.List;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+final class Options {
+
+	private static final String HELP_DESCRIPTION = Messages.getMessage("HELP_OPTION_DESCRIPTION");
+	private static final String VERSION_DESCRIPTION = Messages.getMessage("VERSION_OPTION_DESCRIPTION");
+	private static final String NO_OF_THREADS_OPTION_DESCRIPTION = Messages.getMessage("NO_OF_THREADS_OPTION_DESCRIPTION");
+	private static final String INCLUDE_OPTION_DESCRIPTION = Messages.getMessage("INCLUDE_OPTION_DESCRIPTION");
+	private static final String TAGS_OPTION_DESCRIPTION = Messages.getMessage("TAGS_OPTION_DESCRIPTION");
+	private static final String TAGS_INT_OPTION_DESCRIPTION = Messages.getMessage("TAGS_INT_OPTION_DESCRIPTION");
+	private static final String TAGS_CHAR_OPTION_DESCRIPTION = Messages.getMessage("TAGS_CHAR_OPTION_DESCRIPTION");
+	private static final String MAX_RECORDS_OPTION_DESCRIPTION = Messages.getMessage("MAX_RECORDS_OPTION_DESCRIPTION");
+	private static final String LOG_OPTION_DESCRIPTION = Messages.getMessage("LOG_OPTION_DESCRIPTION");
+	private static final String MIN_BIN_SIZE_OPTION_DESCRIPTION = Messages.getMessage("MIN_BIN_SIZE_OPTION_DESCRIPTION");
+	private static final String LOG_LEVEL_OPTION_DESCRIPTION = Messages.getMessage("LOG_LEVEL_OPTION_DESCRIPTION");
+	private static final String OUTPUT_FILE_DESCRIPTION = Messages.getMessage("OUTPUT_FILE_DESCRIPTION");
+	private static final String INPUT_FILE_DESCRIPTION = Messages.getMessage("INPUT_FILE_DESCRIPTION");
+	private static final String VALIDATION_STRINGENCY_OPTION_DESCRIPTION = Messages.getMessage("VALIDATION_STRINGENCY_DESCRIPTION");
+	private static final String NO_HTML_DESCRIPTION = Messages.getMessage("NO_HTML_OPTION_DESCRIPTION");
+	
+	private final OptionParser parser = new OptionParser();
+	private final OptionSet options;
+//	private final List<String> fileNames;
+	private final String[] fastqs;
+	private final String outputFileName;
+	private final String log;
+	private final Integer minBinSize;
+	private final String input;
+	private final String logLevel;
+
+	@SuppressWarnings("unchecked")
+	Options(final String[] args) throws Exception {
+//		parser.acceptsAll(asList("h", "help"), HELP_DESCRIPTION);
+//		parser.acceptsAll(asList("v", "version"), VERSION_DESCRIPTION);
+		parser.accepts("help", HELP_DESCRIPTION);
+		parser.accepts("version", VERSION_DESCRIPTION);
+		
+		parser.accepts("input", INPUT_FILE_DESCRIPTION).withRequiredArg().ofType(String.class);
+		parser.accepts("output", OUTPUT_FILE_DESCRIPTION).withRequiredArg().ofType(String.class);
+//		parser.accepts("exclude", EXCLUDES_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class).withValuesSeparatedBy(',');
+		parser.accepts("minBinSize", MIN_BIN_SIZE_OPTION_DESCRIPTION).withRequiredArg().ofType(Integer.class);
+		parser.accepts("log", LOG_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class);
+		parser.accepts("loglevel", LOG_LEVEL_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class);
+		parser.accepts("fastqs", TAGS_CHAR_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class).withValuesSeparatedBy(',');
+		parser.posixlyCorrect(true);
+
+		options = parser.parse(args);
+		
+		// log file
+		log = (String) options.valueOf("log");
+		// logLevel
+		logLevel = (String) options.valueOf("loglevel");
+		
+		minBinSize =  (Integer) options.valueOf("minBinSize");
+		input = (String) options.valueOf("input");
+		
+		// inputs
+		List<String> inputs = (List<String>) options.valuesOf("fastqs");
+		fastqs = new String[inputs.size()];
+		inputs.toArray(fastqs);
+		
+		
+		// output
+		outputFileName = (String) options.valueOf("output");
+		
+		if ( ! options.nonOptionArguments().isEmpty()) {}
+//			throw new IllegalArgumentException(Messages.getMessage("USAGE"));
+		
+//		List<String> nonoptions = options.nonOptionArguments();
+//		fileNames = new String[nonoptions.size()];
+//		nonoptions.toArray(fileNames);
+	}
+
+	boolean hasVersionOption() {
+		return options.has("version");
+	}
+
+	boolean hasHelpOption() {
+		return options.has("help");
+	}
+	boolean hasMinBinSizeOption() {
+		return options.has("minBinSize");
+	}
+	
+	boolean hasNoHtmlOption() {
+		return options.has("nohtml");
+	}
+	
+	boolean hasLogOption() {
+		return options.has("log");
+	}
+	
+	boolean hasLogLevelOption() {
+		return options.has("loglevel");
+	}
+	
+	Integer getMinBinSize() {
+		return minBinSize;
+	}
+	
+	String getInput() {
+		return input;
+	}
+
+	String[] getFastqs() {
+		return fastqs;
+	}
+//	List<String> getFileNames() {
+//		return fileNames;
+//	}
+	
+	
+	String getLog() {
+		return log;
+	}
+	String getLogLevel() {
+		return logLevel;
+	}
+	public String getOutputFileName() {
+		return outputFileName;
+	}
+
+	void displayHelp() throws Exception {
+		parser.printHelpOn(System.err);
+	}
+	
+	String getValidation() {
+		if (options.has("validation")) {
+			return (String) options.valueOf("validation");
+		} else return null;
+	}
+
+}
---
+++ src/au/edu/qimr/tiledaligner/Q3TiledAligner.java
@@ -0,0 +1,167 @@
+package au.edu.qimr.tiledaligner;
+
+import htsjdk.samtools.reference.FastaSequenceFile;
+import htsjdk.samtools.reference.ReferenceSequence;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.qcmg.common.log.QLogger;
+import org.qcmg.common.log.QLoggerFactory;
+import org.qcmg.common.model.ChrPosition;
+import org.qcmg.common.util.FileUtils;
+import org.qcmg.common.util.LoadReferencedClasses;
+
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+
+public class Q3TiledAligner {
+	
+private static QLogger logger;
+	
+	
+	private static String version;
+	private String logFile;
+	private String outputFile;
+	private String inputFile;
+	private FastaSequenceFile sequenceFile;
+	
+	// chr specific data
+	private byte[] referenceBases;
+	private int referenceBasesLength;
+	private String currentChr;
+	
+	private final int tileSize = 13;
+	
+	private final Map<String, List<ChrPosition>> tilesAndPositions = new HashMap<>();
+	
+	private int exitStatus;
+	
+	protected int engage() throws Exception {
+		
+		logger.info("Lets go!");
+				
+		return exitStatus;
+	}
+	
+	void performBinning() {
+		
+		while (true) {
+			// load next contig
+			loadNextReferenceSequence();
+			
+			if (null == referenceBases) {
+				// nothing left - exit
+				break;
+			}
+			
+			for (int i = 0 ; i < referenceBasesLength - tileSize; i++) {
+				String tile = new String(Arrays.copyOfRange(referenceBases, i, i+ tileSize));
+				ChrPosition cp = new ChrPosition(currentChr, i + 1);
+				updateMap(tile, cp);
+			}
+		}
+		
+		// print some stats
+		
+		logger.info("no of entries in tilesAndPositions: " +tilesAndPositions.size());
+	}
+	
+	void updateMap(String tile, ChrPosition pos) {
+		List<ChrPosition> positions = tilesAndPositions.get(tile);
+		if (null == positions) {
+			positions = new ArrayList<>();
+			tilesAndPositions.put(tile, positions);
+		}
+		positions.add(pos);
+	}
+
+	
+	void loadNextReferenceSequence() {
+		if (null == sequenceFile) {
+			sequenceFile = new FastaSequenceFile(new File(inputFile), true);
+		}
+		referenceBases = null;
+		currentChr = null;
+		referenceBasesLength = 0;
+		ReferenceSequence refSeq = sequenceFile.nextSequence();
+		
+		// debugging code
+//		while ( ! "chr1".equals(refSeq.getName()))
+//			refSeq = sequenceFile.nextSequence();
+//			 debugging code
+		if (null == refSeq) {	// end of the line
+			logger.info("No more chromosomes in reference file - shutting down");
+			closeReferenceFile();
+		} else {
+			currentChr = refSeq.getName();
+			referenceBases = refSeq.getBases();
+			referenceBasesLength = refSeq.length();
+			logger.info("Will process records from: " + currentChr + ", length: " + referenceBasesLength);
+		}
+	}
+	
+	void closeReferenceFile() {
+		if (null != sequenceFile) sequenceFile.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// loads all classes in referenced jars into memory to avoid nightly build sheninegans
+		LoadReferencedClasses.loadClasses(Q3TiledAligner.class);
+		
+		Q3TiledAligner qp = new Q3TiledAligner();
+		int exitStatus = qp.setup(args);
+		if (null != logger) {
+			logger.logFinalExecutionStats(exitStatus);
+		} else {
+			System.err.println("Exit status: " + exitStatus);
+		}
+		
+		System.exit(exitStatus);
+	}
+	
+	protected int setup(String args[]) throws Exception{
+		int returnStatus = 1;
+		Options options = new Options(args);
+
+		if (options.hasHelpOption()) {
+			System.err.println(Messages.USAGE);
+			options.displayHelp();
+			returnStatus = 0;
+		} else if (options.hasVersionOption()) {
+			System.err.println(Messages.getVersionMessage());
+			returnStatus = 0;
+		} else if (options.getFastqs().length < 1) {
+			System.err.println(Messages.USAGE);
+//		} else if ( ! options.hasLogOption()) {
+//			System.err.println(Messages.USAGE);
+		} else {
+			// configure logging
+			logFile = options.getLog();
+			version = Q3TiledAligner.class.getPackage().getImplementationVersion();
+			logger = QLoggerFactory.getLogger(Q3TiledAligner.class, logFile, options.getLogLevel());
+			logger.logInitialExecutionStats("q3clinvar", version, args);
+			
+			// get list of file names
+			inputFile = options.getInput();
+			
+			// set outputfile - if supplied, check that it can be written to
+			if (null != options.getOutputFileName()) {
+				String optionsOutputFile = options.getOutputFileName();
+				if (FileUtils.canFileBeWrittenTo(optionsOutputFile)) {
+					outputFile = optionsOutputFile;
+				} else {
+					throw new Exception("OUTPUT_FILE_WRITE_ERROR");
+				}
+			}
+			
+			return engage();
+		}
+		return returnStatus;
+	}
+
+}
