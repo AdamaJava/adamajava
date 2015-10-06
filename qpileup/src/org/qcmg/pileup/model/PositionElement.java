d730b240238ecf581bebea462f63d9de4fe57c7f
holmeso <holmeso@users.noreply.github.com>
2015-10-06 02:15:08 +0000

    removed unused code

--- qpileup/src/org/qcmg/pileup/model/PositionElement.java
+++ qpileup/src/org/qcmg/pileup/model/PositionElement.java
@@ -3,17 +3,14 @@
  */
 package org.qcmg.pileup.model;
 
-import org.qcmg.pileup.PileupConstants;
 
 public class PositionElement {
 	
-	final String chr;
-	final long position;
-	final char base;
-	final static String DELIMITER = PileupConstants.DELIMITER;
+	private final String chr;
+	private final long position;
+	private final char base;
 	
 	public PositionElement(String chr, long position, char base) {
-		super();
 		this.position = position;
 		this.base = base;
 		this.chr = chr;
@@ -22,24 +19,13 @@ public class PositionElement {
 	public long getPosition() {
 		return position;
 	}
-//	public void setPosition(long position) {
-//		this.position = position;
-//	}
 	public char getBase() {
 		return base;
 	}
-//	public void setBase(char base) {
-//		this.base = base;
-//	}	
 	
 	public String getChr() {
 		return chr;
 	}
-
-//	public void setChr(String chr) {
-//		this.chr = chr;
-//	}
-	
 	@Override
 	public String toString() {
 		return chr + "," + base + "," + position;
