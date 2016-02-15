35cbc9dd9c0e57b4648a34e1744cd90bd76d9eb1
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2016-02-15 23:37:41 +0000

    resolving RecordsAtPositionTest.java

--- trunk/adamajava/qsignature/test/org/qcmg/sig/model/ComparisonTest.java
+++ trunk/adamajava/qsignature/test/org/qcmg/sig/model/ComparisonTest.java
@@ -15,52 +15,52 @@ public class ComparisonTest {
 	@Test
 	public void testComparisonConstructor() {
 		try {
-			new Comparison(null,0, null, 0, 0, 0, 0);
+			new Comparison(null,0, null, 0, 0, 0, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		
-		Comparison c = new Comparison(F1, 0, F2, 0, 0, 0, 0);
+		Comparison c = new Comparison(F1, 0, F2, 0, 0, 0, 0, 0, 0);
 		assertEquals(Double.NaN, c.getScore(), 0.00001);
 		
 		try {
-			c = new Comparison(F1, 0, F2, 0, 100, 0, 0);
+			c = new Comparison(F1, 0, F2, 0, 100, 0, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		
 		try {
-			c = new Comparison(F1, 0, F2, 0, -1, 0, 0);
+			c = new Comparison(F1, 0, F2, 0, -1, 0, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		try {
-			c = new Comparison(F1, -1, F2, 0, 0, 0, 0);
+			c = new Comparison(F1, -1, F2, 0, 0, 0, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		try {
-			c = new Comparison(F1, 0, F2, -1, 0, 0, 0);
+			c = new Comparison(F1, 0, F2, -1, 0, 0, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		try {
-			c = new Comparison(F1, 0, F2, 0, 0, -1, 0);
+			c = new Comparison(F1, 0, F2, 0, 0, -1, 0, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 		try {
-			c = new Comparison(F1, 0, F2, 0, 0, 0, -1);
+			c = new Comparison(F1, 0, F2, 0, 0, 0, -1, 0, 0);
 			Assert.fail("Should have thrown an IAE");
 		}	catch (IllegalArgumentException iae){}
 	}
 	
 	@Test
 	public void testComparison() {
-		Comparison c = new Comparison(F1, 0, F2, 0, 0, 1, 0);
+		Comparison c = new Comparison(F1, 0, F2, 0, 0, 1, 0, 0, 0);
 		assertEquals(0.0, c.getScore(), 0.00001);
-		c = new Comparison(F1, 0, F2, 0, 10, 1, 0);
+		c = new Comparison(F1, 0, F2, 0, 10, 1, 0, 0, 0);
 		assertEquals(10.0, c.getScore(), 0.00001);
-		c = new Comparison(F1, 0, F2, 0, 10, 20, 0);
+		c = new Comparison(F1, 0, F2, 0, 10, 20, 0, 0, 0);
 		assertEquals(0.5, c.getScore(), 0.00001);
-		c = new Comparison(F1, 0, F2, 0, 10, 1000, 0);
+		c = new Comparison(F1, 0, F2, 0, 10, 1000, 0, 0, 0);
 		assertEquals(0.01, c.getScore(), 0.00001);
 		
-		c = new Comparison(F1, 123456, F2, 234567, 99999, 123456, 1234560);
+		c = new Comparison(F1, 123456, F2, 234567, 99999, 123456, 1234560, 0, 0);
 		assertEquals((99999.0 / 123456), c.getScore(), 0.00001);
 		assertEquals(F1, c.getMain());
 		assertEquals(F2, c.getTest());
@@ -75,14 +75,14 @@ public class ComparisonTest {
 	
 	@Test
 	public void checkComparator() {
-		Comparison c1 = new Comparison(F1, 0, F2, 0, 10, 100, 0);
-		Comparison c2 = new Comparison(F1, 0, F2, 0, 20, 100, 0);
+		Comparison c1 = new Comparison(F1, 0, F2, 0, 10, 100, 0, 0, 0);
+		Comparison c2 = new Comparison(F1, 0, F2, 0, 20, 100, 0, 0, 0);
 		assertEquals(-1, c1.compareTo(c2));
 		
-		c1 = new Comparison(F1, 0, F2, 0, 20, 100, 0);
+		c1 = new Comparison(F1, 0, F2, 0, 20, 100, 0, 0, 0);
 		assertEquals(0, c1.compareTo(c2));
 		
-		c1 = new Comparison(F1, 0, F2, 0, 200, 1000, 0);
+		c1 = new Comparison(F1, 0, F2, 0, 200, 1000, 0, 0, 0);
 		assertEquals(1000 - 100, c1.compareTo(c2));
 	}
 
