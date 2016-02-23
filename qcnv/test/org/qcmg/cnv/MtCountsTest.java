e85247d7d0f55f5b48d0155a3342d00083a44b51
adama test <christina.xu@qimrberghofer.edu.au>
2016-02-23 05:17:25 +0000

    import qbamfilter into qcnv with unit test

--- qcnv/test/org/qcmg/cnv/MtCountsTest.java
+++ qcnv/test/org/qcmg/cnv/MtCountsTest.java
@@ -1,6 +1,9 @@
 package org.qcmg.cnv;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
@@ -14,6 +17,7 @@ import htsjdk.samtools.SAMRecord;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.qcmg.common.commandline.Executor;
 import org.qcmg.common.log.QLogger;
 import org.qcmg.picard.SAMFileReaderFactory;
 import org.qcmg.picard.SAMOrBAMWriterFactory;
@@ -21,14 +25,17 @@ import org.qcmg.picard.SAMOrBAMWriterFactory;
 public class MtCountsTest {
 	
 	public static final String[] INPUTs = { "input.normal.bam","input.tumor.bam"};
-	 
+	public static final String output = "output.counts";
+ 	
+	String dir = new java.io.File( "." ).getAbsolutePath() + "/";
 
 	@After
-	public void deleteIO(){
-		for (String f : INPUTs)
-			new File(f).delete();
-	 
-		
+	public void deleteIO() throws IOException{
+		File dir = new java.io.File( "." ).getCanonicalFile();		
+		for(File f: dir.listFiles())
+		    if(  f.getName().startsWith("input") || f.getName().startsWith("output") )
+		    	f.delete();
+	 		
 	}
 	@Before
 	public void createIO(){
@@ -36,12 +43,75 @@ public class MtCountsTest {
 	}
 	
 	@Test
-	public void main() throws Exception{
-		String output = "output.counts";
-		String[] ids = {"tumor" ,"normal"};
-		String[] args = {"-i", INPUTs[0],"-i", INPUTs[1], "--id", ids[0] , "--id", ids[1], "-o" , output};
-		Main.main(args);
+	public void testNoQuery() throws Exception{
 		
+		String[] ids = new String[] {"tumor" ,"normal"};
+		String[] args = new String[] {"-i",  INPUTs[0], "-i", INPUTs[1], "--id", ids[0] , "--id", ids[1], "-o" , output};
+		 		
+        Options option = new Options(args);        	
+		MtCounts cnvThread = new MtCounts(option.getInputNames(), option.getSampleIds(), 
+				option.getOutputName(), option.getThreadNumber(), option.getWindowSize(), option.getQuery(),  option.getLogger(args));			
+		cnvThread.callCounts();	
+		
+		String content;	
+		int lineNo = 0; 
+	    try (Scanner scanner = new Scanner(new FileReader(new File(output))).useDelimiter("\\n") ) {
+	    	while(scanner.hasNext()){
+	    		lineNo ++;	    		
+	    		content = scanner.next();		    		
+	    		if(content.startsWith("GL000195.1")){
+	    			System.out.println("GL000195.1 " + lineNo + " " + content);
+	    			assertTrue(content.endsWith("\t0\t0"));	    		
+	    		}else if(content.startsWith("GL000196.1")){
+	    			 String[] eles = content.split("\t");
+	    			 if(lineNo == 21){
+	    				 assertTrue(eles[5].equals("8"));
+	    				 assertTrue(eles[6].equals("4"));
+	    			 }else if(lineNo == 24){
+	    				 assertTrue(eles[5].equals("1"));
+	    				 assertTrue(eles[6].equals("1"));
+	    			 }else{
+	    				 assertTrue(eles[5].equals("0"));
+	    				 assertTrue(eles[6].equals("0"));	    				 
+	    			 }
+	    		} 
+	    	} //end while	    	
+	    } //end try		
 	}
+	
+	@Test
+	public void testQuery() throws Exception{
+						 
+		String[] ids = {"tumor" ,"normal"};
+		String[] args = {"-i", INPUTs[0],"-i", INPUTs[1], "--id", ids[0] , "--id", ids[1], "-o" , output, "-q", "and(RNAME =~ GL00019*, cigar_M >= 50)"};
+		
+        Options option = new Options(args);        	
+		MtCounts cnvThread = new MtCounts(option.getInputNames(), option.getSampleIds(), 
+				option.getOutputName(), option.getThreadNumber(), option.getWindowSize(), option.getQuery(),  option.getLogger(args));			
+		cnvThread.callCounts();	
+		
+		String content;	
+		int lineNo = 0;
+	    try (Scanner scanner = new Scanner(new FileReader(new File(output))).useDelimiter("\\n") ) {
+	    	while(scanner.hasNext()){	 
+	    		lineNo ++;
+	    		content = scanner.next();
+	    		if(content.startsWith("GL000195.1"))
+	    			assertTrue(content.endsWith("\t0\t0"));	    			    		
+	    		else if(content.startsWith("GL000196.1")){
+	    			 String[] eles = content.split("\t");
+	    			 if(lineNo == 21){
+	    				 assertTrue(eles[5].equals("3"));
+	    				 assertTrue(eles[6].equals("3"));
+	    			 }else{
+	    				 assertTrue(eles[5].equals("0"));
+	    				 assertTrue(eles[6].equals("0"));	    				 
+	    			 }
+	    		} 
+	    	} //end while	    	
+	    }//end try
+		
+	}	
+	
 
 }
