0d2e35d1826aecee999f612fa0c2336d2183e80c
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2016-03-14 04:05:31 +0000

    new double array comparison method based on genotype

--- trunk/adamajava/qsignature/src/org/qcmg/sig/util/ComparisonUtil.java
+++ trunk/adamajava/qsignature/src/org/qcmg/sig/util/ComparisonUtil.java
@@ -4,13 +4,23 @@
 package org.qcmg.sig.util;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.qcmg.common.log.QLogger;
+import org.qcmg.common.log.QLoggerFactory;
+import org.qcmg.common.model.ChrPosition;
+import org.qcmg.sig.QSigCompareDistance;
 import org.qcmg.sig.model.Comparison;
 
 public class ComparisonUtil {
 	
 	public static final int MINIMUM_NO_OF_CALCULATIONS = 25000;
+	
+	private static final QLogger logger = QLoggerFactory.getLogger(ComparisonUtil.class);
+	
 	
 	public static String getComparisonsBody(List<Comparison> comparisons) {
 		if (null == comparisons || comparisons.isEmpty()) throw new IllegalArgumentException("null or empty list of comparisons to headerise");
@@ -42,5 +52,109 @@ public class ComparisonUtil {
 		}
 		return false;
 	}
+
+	public static Comparison compareRatiosUsingSnps(final Map<ChrPosition, double[]> file1Ratios,
+				final Map<ChrPosition, double[]> file2Ratios, File file1, File file2, Map<ChrPosition, ChrPosition> positionsOfInterest) {
+			
+			if (null == file1Ratios || null == file2Ratios)
+				throw new IllegalArgumentException("null maps passed to compareRatios");
+			
+			if (null == file1 || null == file2)
+				throw new IllegalArgumentException("null files passed to compareRatios");
+			
+			if (file1Ratios.isEmpty() || file2Ratios.isEmpty()) {
+				return  new Comparison(file1, file1Ratios.size(), file2, file2Ratios.size(), 0, 0, 0, 0, 0);
+			}
+			
+			// if the files are the same, return a comparison object without doing the comparison
+			if (file1.equals(file2)) {
+				return  new Comparison(file1, file1Ratios.size(), file2, file2Ratios.size(), 0, file1Ratios.size(), 0, 0, 0);
+			}
+			
+			boolean checkPositionsList = null != positionsOfInterest && ! positionsOfInterest.isEmpty();
+			
+			int match = 0;
+			int totalCompared = 0;
+			long f1TotalCov = 0;
+			long f2TotalCov = 0;
+			for (Entry<ChrPosition, double[]> file1RatiosEntry : file1Ratios.entrySet()) {
+				
+				// check to see if position is in the list of desired positions, if not continue
+				if (checkPositionsList) {
+					boolean overlap = positionsOfInterest.containsKey(file1RatiosEntry.getKey());
+					if ( ! overlap)  {
+						continue;
+					}
+				}
+				
+				// if coverage is zero, skip
+				final double[] file1Ratio = file1RatiosEntry.getValue();
+				
+				/*
+				 * See if we can get a clear genotype from file1Ratios before retrieving ratios from file2
+				 */
+				int g1 = 0;
+	//			String genotype = "";
+				for (int i = 0 ; i < 4 ; i++) {
+					double d = file1Ratio[i];
+					if (d > 0.90000) {
+						// this is all we care about
+	//					genotype += i + "";
+						g1 += i == 0 ? 1 : i == 1 ? 10 : i == 2 ? 100 : 1000;
+						break;
+					} else if (d >= 0.40000 && d <= 0.60000) {
+						g1 += i == 0 ? 1 : i == 1 ? 10 : i == 2 ? 100 : 1000;
+	//					genotype += i + "";
+					}
+				}
+	//			System.out.println("file1 g1: " + g1);
+	//			System.out.println("file1 genotype: " + genotype + ", g1: " + g1);
+				
+				if (g1 > 0) {
+	//				if ( ! genotype.isEmpty()) {
+					
+					final double[] file2Ratio = file2Ratios.get(file1RatiosEntry.getKey());
+					if (null != file2Ratio) {
+	//					String genotype2 = "";
+						int g2 = 0;
+						for (int i = 0 ; i < 4 ; i++) {
+							double d = file2Ratio[i];
+							if (d > 0.90000) {
+								// this is all we care about
+	//							genotype2 += i + "";
+								g2 += i == 0 ? 1 : i == 1 ? 10 : i == 2 ? 100 : 1000;
+								break;
+							} else if (d >= 0.40000 && d <= 0.60000) {
+	//							genotype2 += i + "";
+								g2 += i == 0 ? 1 : i == 1 ? 10 : i == 2 ? 100 : 1000;
+							}
+						}
+	//					System.out.println("file2 g2: " + g2);
+	//					System.out.println("file2 genotype2: " + genotype2 + ", g2: " + g2);
+						
+						if ( g2 > 0) {
+							if (g1 == g2) {
+								match++;
+							}
+	//						if ( ! genotype2.isEmpty()) {
+	//							if (genotype.equals(genotype2)) {
+	//								match++;
+	//							}
+							totalCompared++;
+							
+							f1TotalCov += file1Ratio[4];
+							f2TotalCov += file2Ratio[4];
+						}
+					}
+				} else {
+//					logger.info("file1Ratios: " + Arrays.toString(file1Ratio));
+				}
+			}
+			double concordance = totalCompared > 0 ? ((double)match / totalCompared) * 100 : 0;
+			logger.info("match: " + match + ", totalCompared: " + totalCompared + " percentage concordance: " + concordance);
+			
+			Comparison comp = new Comparison(file1, file1Ratios.size(), file2, file2Ratios.size(), match, totalCompared, 0, f1TotalCov, f2TotalCov);
+			return comp;
+		}
 
 }
--- trunk/adamajava/qsignature/test/org/qcmg/sig/util/ComparisonUtilTest.java
+++ trunk/adamajava/qsignature/test/org/qcmg/sig/util/ComparisonUtilTest.java
@@ -4,16 +4,97 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.qcmg.common.model.ChrPosition;
+import org.qcmg.common.util.ChrPositionUtils;
 import org.qcmg.sig.model.Comparison;
 
 public class ComparisonUtilTest {
 	
 	static File F1 = new File("f1");
 	static File F2 = new File("f2");
+	
+	double[] allAs = new double[] {1,0,0,0,20};
+	double[] allCs = new double[] {0,1,0,0,20};
+	double[] allGs = new double[] {0,0.0,1,0,20};
+	double[] allTs = new double[] {0,0,0,1,20};
+	
+	double[] asAndCs = new double[] {0.48,0.52,0,0,40};
+	double[] asAndGs = new double[] {0.5,0,0.5,0,40};
+	double[] asAndTs = new double[] {0.4,0,0,0.6,40};
+	
+	double[] csAndGs = new double[] {0,0.4789,0.5123,0,35};
+	
+	
+	@Test
+	public void doesTheSnpMethodWorkSame() {
+		ChrPosition cp = ChrPositionUtils.getChrPosition("1", 100, 100);
+		Map<ChrPosition, double[]> file1Ratios = new HashMap<>();
+		file1Ratios.put(cp,allAs);
+		Map<ChrPosition, double[]> file2Ratios = new HashMap<>();
+		file2Ratios.put(cp,allAs);
+		
+		Comparison comp = ComparisonUtil.compareRatiosUsingSnps(file1Ratios, file2Ratios, new File("blah1"), new File("blah2"), null);
+		assertEquals(1, comp.getScore(), 0.0001);
+		
+		for (int i = 0 ; i < 1000000 ; i++) {
+			cp = ChrPositionUtils.getChrPosition("2", i, i);
+			int mod = i % 16;
+			double [] arrayToUse = mod > 10 ? allCs : (mod > 5) ? asAndTs : csAndGs;
+			file1Ratios.put(cp,arrayToUse);
+			file2Ratios.put(cp,arrayToUse);
+		}
+		
+		comp = ComparisonUtil.compareRatiosUsingSnps(file1Ratios, file2Ratios, new File("blah1"), new File("blah2"), null);
+		assertEquals(1, comp.getScore(), 0.0001);
+	}
+	
+	@Test
+	public void doesTheSnpMethodWorkDifferent() {
+		ChrPosition cp = ChrPositionUtils.getChrPosition("1", 100, 100);
+		Map<ChrPosition, double[]> file1Ratios = new HashMap<>();
+		file1Ratios.put(cp,allAs);
+		Map<ChrPosition, double[]> file2Ratios = new HashMap<>();
+		file2Ratios.put(cp,allCs);
+		
+		Comparison comp = ComparisonUtil.compareRatiosUsingSnps(file1Ratios, file2Ratios, new File("blah1"), new File("blah2"), null);
+		assertEquals(0, comp.getScore(), 0.0001);
+		
+		for (int i = 0 ; i < 1000000 ; i++) {
+			cp = ChrPositionUtils.getChrPosition("2", i, i);
+			file1Ratios.put(cp,allAs);
+			file2Ratios.put(cp,allCs);
+		}
+		
+		comp = ComparisonUtil.compareRatiosUsingSnps(file1Ratios, file2Ratios, new File("blah1"), new File("blah2"), null);
+		assertEquals(0, comp.getScore(), 0.0001);
+	}
+	
+	@Test
+	public void doesTheSnpMethodWorkMixed() {
+		ChrPosition cp = null;
+		Map<ChrPosition, double[]> file1Ratios = new HashMap<>();
+		Map<ChrPosition, double[]> file2Ratios = new HashMap<>();
+		
+		for (int i = 0 ; i < 1000000 ; i++) {
+			cp = ChrPositionUtils.getChrPosition("2", i, i);
+			
+			int mod = i % 16;
+			double [] arrayToUse = mod > 7 ? asAndCs : csAndGs;
+			
+			file1Ratios.put(cp,arrayToUse);
+			file2Ratios.put(cp,mod > 7 ? arrayToUse : allTs);
+		}
+		
+		Comparison comp = ComparisonUtil.compareRatiosUsingSnps(file1Ratios, file2Ratios, new File("blah1"), new File("blah2"), null);
+		assertEquals(0.5, comp.getScore(), 0.0001);
+	}
+	
 	
 	@Test
 	public void testGetComparisonsBody() {
