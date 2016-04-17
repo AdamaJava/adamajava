25658823c41de7e6c1792fde7e2a07d7398b24a1
holmeso <holmeso@users.noreply.github.com>
2016-04-17 22:59:35 +0000

    add const

--- qmotif/src/org/qcmg/motif/util/MotifConstants.java
+++ qmotif/src/org/qcmg/motif/util/MotifConstants.java
@@ -10,6 +10,7 @@ public class MotifConstants {
 	public static final String STAGE_1_STRING_REV_COMP = "stage1_string_rev_comp";
 	public static final String STAGE_2_STRING_REV_COMP = "stage2_string_rev_comp";
 	public static final String INCLUDES_ONLY_MODE = "includes_only";
+	public static final String BASES_CONTAINING_MOTIFS = "bases_containing_motifs";
 	public static final String UNMAPPED = "unmapped";
 
 }
