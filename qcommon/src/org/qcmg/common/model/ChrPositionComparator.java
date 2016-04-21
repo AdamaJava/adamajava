8de538c66d0cf43d0e8d21e427a44cfed9d11a7d
holmeso <holmeso@users.noreply.github.com>
2016-04-21 03:17:37 +0000

    added methods to get custom ChrPosition comparators

--- qcommon/src/org/qcmg/common/model/ChrPositionComparator.java
+++ qcommon/src/org/qcmg/common/model/ChrPositionComparator.java
@@ -1,6 +1,7 @@
 package org.qcmg.common.model;
 
 import java.util.Comparator;
+import java.util.List;
 
 public class ChrPositionComparator implements Comparator<ChrPosition> {
 
@@ -19,5 +20,39 @@ public class ChrPositionComparator implements Comparator<ChrPosition> {
 		return  o1.getEndPosition() - o2.getEndPosition();
 	}
 	
+	/**	
+	 * Creates a ChrPosition comparator that is based on the chromosome name comparator supplied as an argument.
+	 * Allows the the user to be flexible as to how ChrPosition objects are compared
+	 * @param chrNameComp
+	 * @return
+	 */
+	public static Comparator<ChrPosition> getComparator(Comparator<String> chrNameComp) {
+		
+		return Comparator.comparing(ChrPosition::getChromosome, chrNameComp)
+				.thenComparingInt(ChrPosition::getStartPosition)
+				.thenComparingInt(ChrPosition::getEndPosition);
+	}
+	
+	
+	/**
+	 * This method is useful if you have a list of contigs whose order you want to preserve.
+	 * eg. a sorted bam will in its  header have a list of contigs, and it is possible that you would like to sort chromosome (Strings) based on this order
+	 * 
+	 * If the list is empty of null, then then @link ReferenceNameComparator comparator will be returned.
+	 * 
+	 * @param chrNameComp
+	 * @return
+	 */
+	public static Comparator<String> getChrNameComparator(List<String> list) {
+		
+		return	(null == list || list.isEmpty()) ? COMPARATOR : 
+			new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return list.indexOf(o1) - list.indexOf(o2);
+			}
+		};
+		
+	}
 
 }
