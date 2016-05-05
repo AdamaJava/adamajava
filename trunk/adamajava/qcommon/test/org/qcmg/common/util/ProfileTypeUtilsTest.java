650a66d09627102445e392466c80c7ffe951e1da
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2016-05-05 03:24:52 +0000

    extra tests

--- trunk/adamajava/qcommon/test/org/qcmg/common/util/ProfileTypeUtilsTest.java
+++ trunk/adamajava/qcommon/test/org/qcmg/common/util/ProfileTypeUtilsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.qcmg.common.model.ProfileType;
 
@@ -15,11 +16,28 @@ public class ProfileTypeUtilsTest {
 		assertEquals(ProfileType.FASTQ, ProfileTypeUtils.getType(new File("blah.fq")));
 		assertEquals(ProfileType.FASTQ, ProfileTypeUtils.getType(new File("blah.fastq.gz")));
 		assertEquals(ProfileType.FASTQ, ProfileTypeUtils.getType(new File("blah.fastq")));
+		try {
+			ProfileTypeUtils.getType(new File("blah.faq.gz"));
+			Assert.fail("should have barfed");
+		} catch (Exception e) {}
 	}
 	@Test
 	public void gffs() throws Exception {
 		assertEquals(ProfileType.GFF, ProfileTypeUtils.getType(new File("blah.gff")));
 		assertEquals(ProfileType.GFF, ProfileTypeUtils.getType(new File("blah.gff3")));
+		try {
+			ProfileTypeUtils.getType(new File("blah.gff2"));
+			Assert.fail("should have barfed");
+		} catch (Exception e) {}
+	}
+	@Test
+	public void bams() throws Exception {
+		assertEquals(ProfileType.BAM, ProfileTypeUtils.getType(new File("blah.sam")));
+		assertEquals(ProfileType.BAM, ProfileTypeUtils.getType(new File("blah.bam")));
+		try {
+			ProfileTypeUtils.getType(new File("blah.bam123"));
+			Assert.fail("should have barfed");
+		} catch (Exception e) {}
 	}
 
 }
