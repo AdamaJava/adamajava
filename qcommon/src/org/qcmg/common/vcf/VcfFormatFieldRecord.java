62d4214e8b28d84ea7d8b642deeddb402c823ad3
holmeso <holmeso@users.noreply.github.com>
2016-06-13 04:14:55 +0000

    integrated licensing branch

--- qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
+++ qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
@@ -1,3 +1,8 @@
+/**
+ * © Copyright QIMR Berghofer Medical Research Institute 2014-2016.
+ *
+ * This code is released under the terms outlined in the included LICENSE file.
+*/
 package org.qcmg.common.vcf;
 
 import java.util.ArrayList;
--- qsignature/src/org/qcmg/sig/model/RecordsAtPosition.java
+++ qsignature/src/org/qcmg/sig/model/RecordsAtPosition.java
@@ -1,5 +1,8 @@
 /**
- * © Copyright The University of Queensland 2010-2014.  This code is released under the terms outlined in the included LICENSE file.
+ * © Copyright The University of Queensland 2010-2014.
+ * © Copyright QIMR Berghofer Medical Research Institute 2014-2016.
+ *
+ * This code is released under the terms outlined in the included LICENSE file.
  */
 package org.qcmg.sig.model;
 
