32a72b6d1e928d02e0627c5486bcc246cff5e42a
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2016-08-08 05:17:13 +0000

    added ctor args for async output settings

--- trunk/adamajava/qpicard/src/org/qcmg/picard/SAMOrBAMWriterFactory.java
+++ trunk/adamajava/qpicard/src/org/qcmg/picard/SAMOrBAMWriterFactory.java
@@ -7,17 +7,25 @@
 package org.qcmg.picard;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.qcmg.common.log.QLogger;
+import org.qcmg.common.log.QLoggerFactory;
+
 import htsjdk.samtools.BAMIndex;
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SAMFileWriter;
 import htsjdk.samtools.SAMFileWriterFactory;
+import htsjdk.samtools.SAMFileWriterImpl;
 
 public class SAMOrBAMWriterFactory {
+	
+	private static final QLogger logger = QLoggerFactory.getLogger(SAMOrBAMWriterFactory.class);
+	
 	private final File output;
 	private final SAMFileWriter writer;
 	private final boolean index;
@@ -43,25 +51,65 @@ public class SAMOrBAMWriterFactory {
 		this(header, preSort, output,tmpDir, 0, createindex);
 	}
 	
-	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int RamReads){
-		this(header, preSort, output,tmpDir, RamReads, true);
+	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int ramReads){
+		this(header, preSort, output,tmpDir, ramReads, true);
 	}
-	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int RamReads, boolean createindex){
+	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int ramReads, boolean createIndex){
+		this(header, preSort, output,tmpDir, ramReads, createIndex, false);
+	}
+	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int ramReads, boolean createIndex, boolean useAsyncIO){
+		this(header, preSort, output,tmpDir, ramReads, createIndex, useAsyncIO, -1);
+	}
+	
+	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int ramReads, boolean createIndex, boolean useAsyncIO, int asyncOutputBufferSize){
 		SAMFileWriterFactory factory = new SAMFileWriterFactory();   
-		if(RamReads != 0)
-			htsjdk.samtools.SAMFileWriterImpl.setDefaultMaxRecordsInRam( RamReads );
+		if (ramReads > 0) {
+			htsjdk.samtools.SAMFileWriterImpl.setDefaultMaxRecordsInRam( ramReads );
+		}
 		
-		index = createindex && header.getSortOrder().equals(SAMFileHeader.SortOrder.coordinate);
+		index = createIndex && header.getSortOrder().equals(SAMFileHeader.SortOrder.coordinate);
 		
-		factory.setCreateIndex(index);	
+		factory.setCreateIndex(index);
 		
-		if(tmpDir != null)
+		/*
+		 * set async setting
+		 */
+		factory.setUseAsyncIo(useAsyncIO);
+		if (useAsyncIO && asyncOutputBufferSize > 0) {
+			factory.setAsyncOutputBufferSize(asyncOutputBufferSize);
+		}
+		
+		if (tmpDir != null) {
 			factory.setTempDirectory(tmpDir);
+		}
 		
 		this.output = output;		
 		
 		writer = factory.makeSAMOrBAMWriter(header, preSort, output);  
 	}
+//	public SAMOrBAMWriterFactory(SAMFileHeader header,  boolean preSort, File output,File tmpDir, int ramReads, boolean createindex){
+//		SAMFileWriterFactory factory = new SAMFileWriterFactory();   
+//		if(ramReads != 0) {
+//			htsjdk.samtools.SAMFileWriterImpl.setDefaultMaxRecordsInRam( ramReads );
+//		}
+//		
+//		index = createindex && header.getSortOrder().equals(SAMFileHeader.SortOrder.coordinate);
+//		
+//		factory.setCreateIndex(index);
+//		
+//		/*
+//		 * set to async
+//		 */
+//		factory.setUseAsyncIo(true);
+//		factory.setAsyncOutputBufferSize(SAMFileWriterImpl.getDefaultMaxRecordsInRam());
+//		if(tmpDir != null) {
+//			factory.setTempDirectory(tmpDir);
+//		}
+//		
+//		this.output = output;		
+//		
+//		writer = factory.makeSAMOrBAMWriter(header, preSort, output);  
+//	}
 	
 	
 	public SAMFileWriter getWriter(){
@@ -71,15 +119,17 @@ public class SAMOrBAMWriterFactory {
 	public void closeWriter(){
 		writer.close();
  
-		if(index)
+		if (index) {
 			renameIndex(output);
+		}
 	}
 	
 	/**
 	 * rename index, eg. output.bai to output.bam.bai if the related bam named as output.bam
 	 * @param bamFile
+	 * @throws IOException 
 	 */
-	public void renameIndex(File bamFile) {
+	public void renameIndex(File bamFile)  {
 		String path = bamFile.getPath();
 	   	String indexFileBase = bamFile.getPath().endsWith(".bam") ? bamFile.getPath().substring(0, path.lastIndexOf(".")) : path;
 
@@ -96,8 +146,9 @@ public class SAMOrBAMWriterFactory {
 	        	try{
 					Files.move(pOrg, pDes, StandardCopyOption.REPLACE_EXISTING);			
 					org.delete();			 
-				}catch(Exception e){
+				}catch(IOException e){
 					logMessage ="Error: Exception occured during deleting file: " + org.getPath();
+					logger.error("IOEXception caught whilst trying to move file", e);
 				}
 	        }
 	        logMessage = "Succeed: renamed index file to " + des.getPath();
