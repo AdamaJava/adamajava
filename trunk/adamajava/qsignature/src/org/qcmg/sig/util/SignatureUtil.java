e19a4f4b2bae6e45503f75cb8cc83fdce48d6798
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2016-08-23 01:41:19 +0000

    removed old file

--- trunk/adamajava/qsignature/src/org/qcmg/sig/util/SignatureUtil.java
+++ trunk/adamajava/qsignature/src/org/qcmg/sig/util/SignatureUtil.java
@@ -455,24 +455,6 @@ public class SignatureUtil {
 	}
 	
 	
-	public static int sendEmail(String subject, String body, String recipients, QLogger logger) throws IOException, InterruptedException {
-		
-		String[] cmd = { "/bin/bash", "-c", "echo \"" + body + "\" | mail -s " + subject + " " + recipients};
-		if (logger != null) logger.debug(Arrays.deepToString(cmd));
-		
-		Process p = new ProcessBuilder(cmd).start();
-		int emalExitStatus = p.waitFor();
-		
-		byte[] errorStream = new byte[1024];
-		java.io.InputStream isError = p.getErrorStream();
-		isError.read(errorStream);
-		
-		String errorMessage = new String(errorStream);
-		if (logger != null) logger.info("Email sending exit status: " + emalExitStatus + ", msg: " + errorMessage);
-		
-		return emalExitStatus;
-	}
-	
 	/**
 	 * Returns the contents of a file as a list of strings
 	 * Assumes that each file identified within this file is on a single line
