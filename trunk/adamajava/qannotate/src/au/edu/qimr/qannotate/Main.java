c9979c8d5e735dba997edc73f5f2c3558fe75164
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2017-01-11 03:20:44 +0000

    added braces

--- trunk/adamajava/qannotate/src/au/edu/qimr/qannotate/Main.java
+++ trunk/adamajava/qannotate/src/au/edu/qimr/qannotate/Main.java
@@ -19,30 +19,31 @@ public class Main {
 		try {
             final Options options = new Options(args);
                 
-        	LoadReferencedClasses.loadClasses(Main.class);    
+        		LoadReferencedClasses.loadClasses(Main.class);    
            logger = QLoggerFactory.getLogger(Main.class, options.getLogFileName(),  options.getLogLevel());	            		               
            logger.logInitialExecutionStats(options.getPGName(), options.getVersion(),args);	        
            
-           if(options.getMode() == Options.MODE.dbsnp)
-        	   new DbsnpMode( options );
-           else if(options.getMode() == Options.MODE.germline)
-        	   new GermlineMode( options );
-           else if(options.getMode() == Options.MODE.snpeff)
-        	    new SnpEffMode(   options  );
-           else if(options.getMode() == Options.MODE.confidence)
-        	    new ConfidenceMode(   options);
-           else if(options.getMode() == Options.MODE.vcf2maf)
-        	   new Vcf2maf(  options );
-           else if(options.getMode() == Options.MODE.cadd)
-        	   new CaddMode(   options   );
-           else if(options.getMode() == Options.MODE.indelconfidence)
-        	   new IndelConfidenceMode(options);
-           else if(options.getMode() == Options.MODE.hom)
-        	   new HomoplymersMode(options);
-           else if(options.getMode() == Options.MODE.trf)
-        	   new TandemRepeatMode( options );
-           else
+           if (options.getMode() == Options.MODE.dbsnp) {
+        	   		new DbsnpMode( options );
+           	} else if (options.getMode() == Options.MODE.germline) {
+        	   		new GermlineMode( options );
+			} else if (options.getMode() == Options.MODE.snpeff) {
+        	    		new SnpEffMode(   options  );
+			} else if (options.getMode() == Options.MODE.confidence) {
+        	    		new ConfidenceMode(   options);
+			} else if (options.getMode() == Options.MODE.vcf2maf) {
+				new Vcf2maf(  options );
+			} else if (options.getMode() == Options.MODE.cadd) {
+        	   		new CaddMode(   options   );
+           } else if (options.getMode() == Options.MODE.indelconfidence) {
+        	   		new IndelConfidenceMode(options);
+           } else if (options.getMode() == Options.MODE.hom) {
+        	   		new HomoplymersMode(options);
+           } else if (options.getMode() == Options.MODE.trf) {
+        	   		new TandemRepeatMode( options );
+           } else {
             	   throw new Exception("No valid mode are specified on commandline: " + options.getMode().name()) ;
+           }
 
             logger.logFinalExecutionStats(0);
                
@@ -52,7 +53,7 @@ public class Main {
 	        	System.err.println(Thread.currentThread().getName() + " " + e.toString());
 	        	if (null != logger) {
 		        	logger.info(Thread.currentThread().getName() + " " + e.toString());	            
-		            logger.logFinalExecutionStats(1);
+		        logger.logFinalExecutionStats(1);
 	        	}
         		System.out.println("About to return exit code of 1");
             System.exit(1);
