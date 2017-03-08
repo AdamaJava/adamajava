afafb6eaf63bad08a1857caa4a4f959857de39b7
holmeso <holmeso@users.noreply.github.com>
2017-03-08 01:30:34 +0000

    delete unused code

--- qprofiler/src/org/qcmg/qprofiler/fasta/FastaSummarizer.java
+++ qprofiler/src/org/qcmg/qprofiler/fasta/FastaSummarizer.java
@@ -20,7 +20,6 @@
 package org.qcmg.qprofiler.fasta;
 
 import java.io.File;
-import java.util.concurrent.BlockingQueue;
 
 import org.qcmg.common.date.DateUtils;
 import org.qcmg.common.log.QLevel;
@@ -36,10 +35,7 @@ import org.qcmg.simple.SimpleFileReader;
 public class FastaSummarizer implements Summarizer {
 	
 	private final static QLogger logger = QLoggerFactory.getLogger(FastaSummarizer.class);
-	private boolean endOfFileReached;
-//	private boolean allRecordsParsed;
 	
-	private Throwable thrown;
 	private String [] excludes;
 	
 	public FastaSummarizer() {}	// default constructor
@@ -82,66 +78,4 @@ public class FastaSummarizer implements Summarizer {
 		return fastaSummaryReport;
 	}
 	
-	public class FileReader implements Runnable {
-		
-		private final SimpleFileReader reader;
-		private final BlockingQueue<Record> queue;
-		
-		public FileReader(SimpleFileReader reader, BlockingQueue<Record> queue) throws Exception {
-			this.queue = queue;
-			this.reader = reader;
-		}
-
-		@Override
-		public void run() {
-			logger.info("Fasta FileReader thread started!!!");
-				
-			long count = 0;
-			try {
-				for (Record record : reader) {
-					queue.put(record);
-					if (++count % 1000000 == 0) {
-//							if (++count % FEEDBACK_LINES_COUNT == 0) {
-						logger.debug("Records parsed in FastaSummarizer: " + count);
-//						logger.debug("Records parsed in FastaSummarizer: {}", count);
-					}
-				}
-			} catch (InterruptedException ie) {
-				logger.warn("FileReader thread interrupted!!!");
-				Thread.currentThread().interrupt();
-			} catch (Exception e) {
-				logger.error("Exception caught by FileReader thread");
-				thrown = e;
-			} finally {
-				endOfFileReached = true;
-				logger.debug("Fasta FileReader thread finished");
-			}
-		}
-	}
-	
-	public static class FASTARecordConsumer implements Runnable {
-		
-		private final BlockingQueue<Record> queue;
-		private final FastaSummaryReport report;
-		
-		public FASTARecordConsumer(FastaSummaryReport report, BlockingQueue<Record> queue) {
-			this.queue = queue;
-			this.report = report;
-		}
-		
-		@Override
-		public void run() {
-			logger.debug("FASTARecordConsumer thread started!!!" );
-			long count = 0;
-			try {
-				while (true) {
-					report.parseRecord((SimpleRecord) queue.take());
-					if (++count % 1000000 == 0)
-						logger.info("count: " + count); 
-				}
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-		}
-	}
 }
