8ea8abed977454fca04f47014cf2e73f07dbb076
qxu-sourceforge <qxu-sourceforge@8d477179-88b4-446a-adef-fc416ee005a4>
2017-03-09 06:05:48 +0000

    fix merge error

--- trunk/adamajava/qprofiler/src/org/qcmg/qprofiler/summarise/SampleSummary.java
+++ trunk/adamajava/qprofiler/src/org/qcmg/qprofiler/summarise/SampleSummary.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
