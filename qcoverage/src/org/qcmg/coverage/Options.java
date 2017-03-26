6f412f9be59818ed9f49f568b9411d57a83e177d
holmeso <holmeso@users.noreply.github.com>
2017-03-26 23:21:52 +0000

    added messages back in

--- qcoverage/src/org/qcmg/coverage/Options.java
+++ qcoverage/src/org/qcmg/coverage/Options.java
@@ -44,8 +44,8 @@ public final class Options {
 			.getMessage("VALIDATION_STRINGENCY_DESCRIPTION");
 	private static final String SEGMENTER_OPTION_DESCRIPTION = Messages
 			.getMessage("SEGMENTER_OPTION_DESCRIPTION");
-	private static final String INPUT_DESCRIPTION = Messages.getMessage("INPUT_DESCRIPTION");
-	private static final String MISSION_OUTPUT_OPTION = Messages.getMessage("MISSING_OUTPUT_OPTIONS");
+	private static final String INPUT_DESCRIPTION = Messages
+			.getMessage("INPUT_DESCRIPTION");
 
 	private final OptionParser parser = new OptionParser();
 	private final OptionSet options;
@@ -71,33 +71,46 @@ public final class Options {
 		parser.acceptsAll(asList("xml"), XML_OPTION_DESCRIPTION);
 		parser.accepts("vcf", VCF_OPTION_DESCRIPTION);
 		parser.acceptsAll(asList("o", "output"), OUTPUT_DESCRIPTION)
-				.withRequiredArg().ofType(String.class).describedAs("outputfile");
+				.withRequiredArg().ofType(String.class).describedAs(
+						"outputfile");
 		parser.acceptsAll(asList("bam"), INPUT_BAM_OPTION_DESCRIPTION)
 				.withRequiredArg().ofType(String.class).describedAs("BAM file");
 		parser.acceptsAll(asList("bai"), INPUT_BAI_OPTION_DESCRIPTION)
 				.withRequiredArg().ofType(String.class).describedAs("BAI file");
 		parser.acceptsAll(asList("gff3"), INPUT_GFF3_OPTION_DESCRIPTION)
-				.withRequiredArg().ofType(String.class).describedAs("GFF3 file");
+				.withRequiredArg().ofType(String.class)
+				.describedAs("GFF3 file");
 		parser.acceptsAll(asList("t", "type"), TYPE_OPTION_DESCRIPTION)
-				.withRequiredArg().ofType(String.class).describedAs("'seq', 'sequence', 'phys', or 'physical'");
+				.withRequiredArg().ofType(String.class).describedAs(
+						"'seq', 'sequence', 'phys', or 'physical'");
 		parser.acceptsAll(asList("q", "query"), QUERY_OPTION_DESCRIPTION)
-				.withRequiredArg().ofType(String.class).describedAs("expression");
+				.withRequiredArg().ofType(String.class).describedAs(
+						"expression");
 		parser.acceptsAll(asList("n"), NUMBER_THREADS_DESCRIPTION)
-				.withRequiredArg().ofType(Integer.class).describedAs("number of worker threads");
-		parser.acceptsAll(asList("per-feature"),PER_FEATURE_OPTION_DESCRIPTION);
+				.withRequiredArg().ofType(Integer.class).describedAs(
+						"number of worker threads");
+		parser
+				.acceptsAll(asList("per-feature"),
+						PER_FEATURE_OPTION_DESCRIPTION);
 		parser.acceptsAll(asList("v", "V", "version"), VERSION_DESCRIPTION);
-		parser.accepts("log", LOG_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class);
-		parser.accepts("loglevel", LOG_LEVEL_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class);
-		parser.accepts("validation", VALIDATION_STRINGENCY_OPTION_DESCRIPTION).withRequiredArg().ofType(String.class); 
+		parser.accepts("log", LOG_OPTION_DESCRIPTION).withRequiredArg().ofType(
+				String.class);
+		parser.accepts("loglevel", LOG_LEVEL_OPTION_DESCRIPTION)
+				.withRequiredArg().ofType(String.class);
+		parser.accepts("validation", VALIDATION_STRINGENCY_OPTION_DESCRIPTION)
+				.withRequiredArg().ofType(String.class); 
 		
 		
 		//segmenter options
 		parser.accepts("segmenter", SEGMENTER_OPTION_DESCRIPTION);
-		parser.accepts("infile", Messages.getMessage(INPUT_DESCRIPTION)).withRequiredArg().ofType(String.class).describedAs("inputfile");
+		parser.accepts("infile", INPUT_DESCRIPTION).withRequiredArg().ofType(String.class).describedAs(
+				"inputfile");
 		parser.acceptsAll(asList("outfile"), Messages
-				.getMessage("INPUT_DESCRIPTION")).withRequiredArg().ofType(String.class).describedAs("inputfile");
+				.getMessage("OUTFILE_DESCRIPTION")).withRequiredArg().ofType(String.class).describedAs(
+				"outputfile");
 		parser.acceptsAll(asList("f", "feature"), Messages
-				.getMessage("FEATURE_DESCRIPTION")).withRequiredArg().ofType(String.class).describedAs("feature");
+				.getMessage("FEATURE_DESCRIPTION")).withRequiredArg().ofType(String.class).describedAs(
+				"feature");
 		parser.accepts("merge", Messages.getMessage("MERGE_OPTION_DESCRIPTION"));
 		parser.accepts("fill", Messages.getMessage("FILL_OPTION_DESCRIPTION"));
 		parser.accepts("bounds", Messages.getMessage("BOUNDS_OPTION_DESCRIPTION")).withRequiredArg().ofType(String.class).describedAs(
@@ -197,6 +210,14 @@ public final class Options {
 		return logLevel;
 	}
 
+	boolean hasJsonFlag() {
+		return options.has("json");
+	}
+
+	public boolean hasInputBAIOption() {
+		return options.has("bai");
+	}
+
 	public boolean hasInputGFF3Option() {
 		return options.has("gff3");
 	}
@@ -215,6 +236,10 @@ public final class Options {
 
 	public boolean hasHelpOption() {
 		return options.has("h") || options.has("help");
+	}
+
+	public boolean hasNonOptions() {
+		return 0 != options.nonOptionArguments().size();
 	}
 
 	public boolean hasNumberThreadsOption() {
@@ -257,20 +282,23 @@ public final class Options {
 		if (!hasLogOption()) {
 			throw new Exception("A log filename must be specified (using the --log option)");
 		}
-		
-		if ( ! hasOutputOption()) {
-			throw new Exception(MISSION_OUTPUT_OPTION);
-		}
-		
 		if (isSegmenterMode) {
-			if ( ! hasOption("infile")) {
+			if (!hasOption("infile")) {
 				throw new Exception("Missing infile option");
 			}
+			
 			checkFile("Input gff3", inputSegmentFile);			
+			
+			if (!hasOption("outfile")) {
+				throw new Exception("Missing outfile option");
+			}			
+			
 			if (!hasOption("feature")) {
 				throw new Exception("Missing feature option");
 			}
+							
 			checkFile("Bounds", bounds);				
+ 			
 			
 		} else {			
 			if (!hasTypeOption()) {
@@ -285,17 +313,22 @@ public final class Options {
 			if (!hasInputGFF3Option()) {
 				throw new Exception("Missing GFF3 input file option");
 			}
+			if (!hasOutputOption()) {
+				throw new Exception("Missing output option");
+			}
 			if (1 != getInputGFF3FileNames().length) {
 				throw new Exception("Only one input GFF3 file should be provided");
 			}
 			if (hasNumberThreadsOption() && 1 < getNumberThreads().length) {
 				throw new Exception("Thread count can be specified once");
 			}
+			
 		}
+		
 	}
 
 	private void checkFile(String type, String fileName) throws Exception {
-		if ( ! new File(fileName).exists()) {
+		if (!new File(fileName).exists()) {
 			throw new Exception(type + " file: " + fileName + " does not exist");
 		}		
 	}
