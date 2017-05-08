b5c0c71d48f78ef56ffafef39b32d573219c2479
holmeso <holmeso@users.noreply.github.com>
2017-05-08 01:54:07 +0000

    perform meaningful tests

--- qcoverage/test/org/qcmg/coverage/QueryPhysicalCoverageTest.java
+++ qcoverage/test/org/qcmg/coverage/QueryPhysicalCoverageTest.java
@@ -1,15 +1,22 @@
 package org.qcmg.coverage;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import htsjdk.samtools.SAMFileHeader.SortOrder;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -18,218 +25,335 @@ import org.qcmg.gff3.GFF3FileWriter;
 import org.qcmg.gff3.GFF3Record;
 
 public class QueryPhysicalCoverageTest {
- 	
+	static String inputBam;
+	static String inputBai;
+	static Path tmpDir;
+	private File fOutput;
+	static GFF3Record record;
 
-	final String inputSam1 = "coverage.sam"; 
-	final String inputBam1 = "coverage.bam";
-	final String inputIndex1 = "coverage.bai";
-	final String inputSam2 = "coverage2.sam"; 
-	final String inputBam2 = "coverage2.bam";
-	final String inputIndex2 = "coverage2.bai";
-	final String output = "output";
-	final String gff3 = "test.gff3";
-	private final String commandline = String.format("--log ./logfile --query ISIZE<50 -t phys --gff3 test.gff3 --bam coverage.bam --bai coverage.bai -o output");
- 	
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
+	
+	 @BeforeClass
+	 public static void setup() throws Exception {
+		 tmpDir = Files.createTempDirectory(null);
+		 inputBam = Files.createTempFile(tmpDir, null, ".bam").toString();
+		 inputBai = inputBam.replace("bam", "bai");
+		 SequenceCoverageTest.createCoverageBam(inputBam, SequenceCoverageTest.getAACSAMRecords(SortOrder.coordinate), SequenceCoverageTest.createSamHeaderObject(SortOrder.coordinate));
+		 
+		 record = new GFF3Record();
+		 record.setSeqId("chr1");
+		 record.setType("exon");
+		 record.setScore(".");
+		 record.setSource(".");
+		 record.setStrand("+");
+	 }
+	 
+	 @AfterClass
+	 public static void tearDown() {
+		tmpDir.toFile().delete();
+	 }
 
 	@Before
 	public final void before() {
-		try {
-			createCoverageSam(inputSam1);
-			MultiBamPhysicalCoverageTest.createCoverageBam(inputSam1, inputBam1 ,inputIndex1 );
-
-			createCoverageSam(inputSam2);
-			MultiBamPhysicalCoverageTest.createCoverageBam(inputSam2, inputBam2 , inputIndex2);
-		} catch (Exception e) {
-			System.err.println("File creation error in test harness: "
-					+ e.getMessage());
-		}
+		fOutput = new File(tmpDir.toString() + "/output");
 	}
 
 	@After
 	public final void after() {
-		try {
-			File file = new File(inputSam1);
-			file.delete();
-			File bamFile = new File(inputBam1);
-			bamFile.delete();
-			File baiFile = new File(inputIndex1);
-			baiFile.delete();
-			
-			file = new File(inputSam2);
-			file.delete();
-			bamFile = new File(inputBam2);
-			bamFile.delete();
-			baiFile = new File(inputIndex2);
-			baiFile.delete();			
-			
-			file = new File("output");
-			file.delete();
-		} catch (Exception e) {
-			System.err.println("File deleting error in test harness: "
-					+ e.getMessage());
-		}
- 
-		
+		fOutput.delete();
+	}
+	
+	private String getCmd(int start, int stop) {
+		return "--log " + tmpDir + "/logfile -t phys --query ISIZE<50 --gff3 " + tmpDir + "/test" + start + "-" + stop + ".gff3 --bam " + inputBam + " --bai " + inputBai + " -o " +fOutput.getAbsolutePath();
+	}
+	private String getExCmd(int start, int stop) {
+		return "--log " + tmpDir + "/logfile -t phys --query ISIZE>50 --gff3 " + tmpDir + "/test" + start + "-" + stop + ".gff3 --bam " + inputBam + " --bai " + inputBai + " -o " +fOutput.getAbsolutePath();
 	}
 
-	private File createGFF3File(final int start, final int end) throws Exception {
-		GFF3Record record = new GFF3Record();
-		record.setSeqId("chr1");
-		record.setType("exon");
+	private File createGFF3File(final int start, final int end) throws IOException {
 		record.setStart(start);
 		record.setEnd(end);
-		record.setScore(".");
-		record.setSource(".");
-		record.setStrand("+");
 
-		File file = new File("test.gff3");
-		GFF3FileWriter writer = new GFF3FileWriter(file);
-		writer.add(record);
-		writer.close();
-		
+		File file = new File(tmpDir + "/test" + start +"-" + end + ".gff3");
+		try (GFF3FileWriter writer = new GFF3FileWriter(file)) {
+			writer.add(record);
+		}
 		return file;
 	}
 
-	private Executor execute(final String command) throws Exception {
+	private Executor execute(final String command) throws IOException, InterruptedException {
 		return new Executor(command, "org.qcmg.coverage.Main");
 	}
 	
-	private void deleteFile(File outputFile) {
-		if (outputFile.exists()) {
-			outputFile.delete();
-		}
-	}
-
     @Test
-	public final void leftDisjointRead() throws Exception {
-		File file = createGFF3File(54000, 54025);
+    public final void leftDisjointRead() throws IOException, InterruptedException {
+    		createGFF3File(54000, 54025);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
-		for (String elem : exec.getErrorStreamConsumer().getLines()) {
-			System.out.println(elem);
-		}
+		Executor exec = execute(getCmd(54000, 54025));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(2, fileContents.size());
+		assertEquals("physical	exon	26	0x", fileContents.get(1));
 	}
+    @Test
+    public final void leftDisjointReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54000, 54025);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54000, 54025));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	26	0x", fileContents.get(1));
+    	
+    }
 
     @Test
-	public final void rightDisjointRead() throws Exception {
-		File file = createGFF3File(54077, 54120);
+	public final void rightDisjointRead() throws IOException, InterruptedException {
+    		createGFF3File(54077, 54120);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54077, 54120));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(2, fileContents.size());
+		assertEquals("physical	exon	44	0x", fileContents.get(1));
+		
 	}
-
+    
+    @Test
+    public final void rightDisjointReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54077, 54120);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54077, 54120));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	44	0x", fileContents.get(1));
+    }
    
     @Test
-	public final void leftOnEndRead() throws Exception {
-		File file = createGFF3File(54000, 54026);
+	public final void leftOnEndRead() throws IOException, InterruptedException {
+    		createGFF3File(54000, 54026);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54000, 54026));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(3, fileContents.size());
+		assertEquals("physical	exon	26	0x", fileContents.get(1));
+		assertEquals("physical	exon	1	1x", fileContents.get(2));
 	}
+    
+    @Test
+    public final void leftOnEndReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54000, 54026);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54000, 54026));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	27	0x", fileContents.get(1));
+    }
 
     @Test
-	public final void rightOnEndRead() throws Exception {
-		File file = createGFF3File(54076, 54120);
+	public final void rightOnEndRead() throws IOException, InterruptedException {
+    		createGFF3File(54076, 54120);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54076, 54120));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();		
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(2, fileContents.size());
+		assertEquals("physical	exon	45	0x", fileContents.get(1));
 	}
+    
+    @Test
+    public final void rightOnEndReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54076, 54120);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54076, 54120));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	45	0x", fileContents.get(1));
+    }
 
     @Test
-	public final void leftOverlapRead() throws Exception {
-		File file = createGFF3File(54000, 54036);
+	public final void leftOverlapRead() throws IOException, InterruptedException {
+    	createGFF3File(54000, 54036);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54000, 54036));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(3, fileContents.size());
+		assertEquals("physical	exon	26	0x", fileContents.get(1));
+		assertEquals("physical	exon	11	1x", fileContents.get(2));
 	}
+    
+    @Test
+    public final void leftOverlapReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54000, 54036);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54000, 54036));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	37	0x", fileContents.get(1));
+    }
 
     @Test
 	public final void rightOverlapRead() throws Exception {
-		File file = createGFF3File(54050, 54120);
+    		createGFF3File(54050, 54120);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54050, 54120));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
+		}
+		
+		assertEquals(3, fileContents.size());
+		assertEquals("physical	exon	50	0x", fileContents.get(1));
+		assertEquals("physical	exon	21	1x", fileContents.get(2));
 	}
+    
+    @Test
+    public final void rightOverlapReadEx() throws Exception {
+	    	createGFF3File(54050, 54120);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54050, 54120));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	71	0x", fileContents.get(1));
+    }
 
     @Test
-	public final void subsetRead() throws Exception {
-		File file = createGFF3File(54030, 54070);
+	public final void subsetRead() throws IOException, InterruptedException {
+   		createGFF3File(54030, 54070);
 
 		ExpectedException.none();
-		Executor exec = execute(commandline);
+		Executor exec = execute(getCmd(54030, 54070));
+		
 		assertTrue(0 == exec.getErrCode());
-
-		File outputFile = new File("output");
-		assertTrue(outputFile.exists());
-
-		deleteFile(outputFile);
-		file.delete();
-	}
-
-	public static final void createCoverageSam(final String fileName) throws Exception {
-		File file = new File(fileName);
-
-		try (OutputStream os = new FileOutputStream(file);
-				PrintStream ps = new PrintStream(os);) {
-
-			ps.println("@HD	VN:1.0	SO:coordinate");
-			ps.println("@RG	ID:ZZ	SM:ES	DS:rl=50	");
-			ps.println("@RG	ID:ZZZ	SM:ES	DS:rl=50	");
-			ps.println("@PG	ID:SOLID-GffToSam	VN:1.4.3");
-			ps.println("@SQ	SN:chr1	LN:100000");
-			ps.println("@SQ	SN:chr2	LN:100000");
-			ps.println("@SQ	SN:chr3	LN:100000");
-			ps.println("1290_738_1025	0	chr1	54026	255	45M5H	*	0	0	AACATTCCAAAAGTCAACCATCCAAGTTTATTCTAAATAGATGTG	!DDDDDDDDDDDDDDDD''DDDDDD9DDDDDDDDD:<3B''DDD!	RG:Z:ZZ	CS:Z:T301130201000212101113201021003302230033233111	CQ:Z:BBB=B:@5?>B9A5?>B?'A49<475%@;6<+;9@'4)+8'1?:>");
-			ps.println("2333_755_492	16	chr2	10103	255	10H40M	*	0	0	CACACCACACCCACACACCACACACCACACCCACACCCAC	!=DD?%+DD<)=DDD<@9)9C:DA.:DD>%%,<?('-,4!	RG:Z:ZZ	CS:Z:T0110001110211110111111111111100111001111	CQ:Z:%/&''(*6'&%+441*%=(31)<9(50=9%%8>?+%;<-1");
-			ps.println("1879_282_595	0	chr3	60775	255	40M10H	*	0	0	TCTAAATTTGTTTGATCACATACTCCTTTTCTGGCTAACA	!DD,*@DDD''DD>5:DD>;DDDD=CDD8%%DA9-DDC0!	RG:Z:ZZ	CS:Z:T0223303001200123211133122020003210323011	CQ:Z:=><=,*7685'970/'437(4<:54*:84%%;/3''?;)(");
+		assertTrue(fOutput.exists());
+		
+		List<String> fileContents;
+		try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+			fileContents = r.lines().collect(Collectors.toList());
 		}
+		
+		assertEquals(2, fileContents.size());
+		assertEquals("physical	exon	41	1x", fileContents.get(1));
 	}
-
-
+    
+    @Test
+    public final void subsetReadEx() throws IOException, InterruptedException {
+	    	createGFF3File(54030, 54070);
+	    	
+	    	ExpectedException.none();
+	    	Executor exec = execute(getExCmd(54030, 54070));
+	    	
+	    	assertTrue(0 == exec.getErrCode());
+	    	assertTrue(fOutput.exists());
+	    	
+	    	List<String> fileContents;
+	    	try (BufferedReader r= new BufferedReader( new FileReader(fOutput))) {
+	    		fileContents = r.lines().collect(Collectors.toList());
+	    	}
+	    	
+	    	assertEquals(2, fileContents.size());
+	    	assertEquals("physical	exon	41	0x", fileContents.get(1));
+    }
 
 }
