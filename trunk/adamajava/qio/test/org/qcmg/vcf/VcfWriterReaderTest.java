68d6a933ffc9cfc2af1711a9e8b1d144759da0a6
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2017-10-30 23:42:03 +0000

    don't throw exception if not required

--- trunk/adamajava/qio/test/org/qcmg/vcf/VcfWriterReaderTest.java
+++ trunk/adamajava/qio/test/org/qcmg/vcf/VcfWriterReaderTest.java
@@ -3,31 +3,30 @@ package org.qcmg.vcf;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.zip.GZIPOutputStream;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.qcmg.common.util.FileUtils;
-import org.qcmg.common.vcf.header.VcfHeaderRecord;
 import org.qcmg.common.vcf.header.VcfHeaderUtils;
 
 public class VcfWriterReaderTest {
 	
+	@Rule
+	public  TemporaryFolder testFolder = new TemporaryFolder();
+	
 	@Test
-	public void testValidation()  {
+	public void testValidation() throws IOException  {
 		String[] fnames = new String[] { "test.output.gz", "test.output" };
 		String[] vcfStrings = new String[] {"#testing...", VcfHeaderUtils.STANDARD_FINAL_HEADER_LINE};
 		for(int i = 0; i < fnames.length; i ++){ 
-			File file = new File(fnames[i]);
+			File file = testFolder.newFile(fnames[i]);
 			//create writer
 			try(VCFFileWriter writer = new VCFFileWriter(file) ){
 				 writer.addHeader(vcfStrings[i]);				
-			} catch (IOException e) { fail(); }
+			} catch (Exception e) { fail(); }
 			
 			//check output type
 			try {
@@ -44,7 +43,7 @@ public class VcfWriterReaderTest {
 				if(i == 0 ) fail();
 				Assert.assertTrue( reader.getHeader() != null);
 			
-			} catch (IOException e) {				 
+			} catch (Exception e) {				 
 				e.printStackTrace();
 				if(i == 1 ) fail();
 				//delete testing file which is invalid vcf, 
@@ -54,25 +53,25 @@ public class VcfWriterReaderTest {
 	}
 
 	@Test
-	public void testCreateAppendVcfWriter()  {
-		File file = new File("output.vcf");
+	public void testCreateAppendVcfWriter() throws IOException {
+		File file =  testFolder.newFile("output.vcf");
 		String[] vcfStrings = new String[] {"##test=test", VcfHeaderUtils.STANDARD_FINAL_HEADER_LINE};
 		
 		//create new file 
 		try(VCFFileWriter writer = new VCFFileWriter(file) ){
 			 writer.addHeader(vcfStrings[0]);				
-		} catch (IOException e) { fail(); }
+		} catch (Exception e) { fail(); }
 		
 		// read throw exception
 		try(VCFFileReader reader = new VCFFileReader(file);){
 			fail();
-		} catch (IOException e) { }
+		} catch (Exception e) { }
 		
 		
 		//append to file
 		try(VCFFileWriter writer = VCFFileWriter.CreateAppendVcfWriter(file)   ){
 			 writer.addHeader(vcfStrings[1]);				
-		} catch (IOException e) { fail(); }
+		} catch (Exception e) { fail(); }
 		
 		//now it become a valid vcf file
 		try(VCFFileReader reader = new VCFFileReader(file);){ 
@@ -80,7 +79,7 @@ public class VcfWriterReaderTest {
 			Assert.assertTrue( reader.getHeader().getAllMetaRecords().size() == 1);	
 			Assert.assertTrue(file.delete());
 			
-		} catch (IOException e) {fail(); }		
+		} catch (Exception e) {fail(); }		
 		
 	}
 
