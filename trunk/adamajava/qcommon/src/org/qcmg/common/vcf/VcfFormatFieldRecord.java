3d10ce6067f79fb7edf480d2fc6888ea89629067
holmeso <holmeso@8d477179-88b4-446a-adef-fc416ee005a4>
2017-11-06 05:10:21 +0000

    revert revert revert

--- trunk/adamajava/qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
+++ trunk/adamajava/qcommon/src/org/qcmg/common/vcf/VcfFormatFieldRecord.java
@@ -5,9 +5,8 @@
 */
 package org.qcmg.common.vcf;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.qcmg.common.string.StringUtils;
@@ -15,7 +14,16 @@ import org.qcmg.common.util.Constants;
 
 public class VcfFormatFieldRecord {
 	
-final Map<String, String> map;
+	final List<String> keys;
+	final List<String> values ;
+	
+	/**
+	 * create an empty record
+	 */
+	public VcfFormatFieldRecord( ){
+		keys = new ArrayList<String>( );
+		values = new ArrayList<String>( );		
+	}
 	
 	/**
 	 * 
@@ -29,15 +37,20 @@ final Map<String, String> map;
 		if (null == sample) {
 			throw new IllegalArgumentException("sample argument passed to VcfFormatFieldRecord ctor can not be null");
 		}
-			
+				
 		final String[] kk = format.split(Constants.COLON_STRING);	
 		final String[] vv= sample.split(Constants.COLON_STRING);
-		map = new LinkedHashMap<>(kk.length * 2);
+		
+		keys = new ArrayList<String>(kk.length * 2);
+		values = new ArrayList<String>(kk.length * 2);
+						
 		
 		for(int i = 0; i < kk.length; i ++) {
-			if ( ! StringUtils.isNullOrEmpty(kk[i])) {
-				map.put(kk[i], vv.length > i ? vv[i] : Constants.MISSING_DATA_STRING);
-			}
+			keys.add(kk[i]);
+			if(vv.length > i)
+				values.add(vv[i]);
+			else
+				values.add(Constants.MISSING_DATA_STRING);
 		}
 	}
 	/**
@@ -51,8 +64,13 @@ final Map<String, String> map;
 		if (null == key) 
 			throw new IllegalArgumentException("null key passed to getField");
 		
-		String value = map.get(key);
-		return (null != value && value.isEmpty()) ? Constants.MISSING_DATA_STRING: value;
+		int index = getKeyIndex(key);		
+		
+		//can't find the key
+		if(index == -1) return null;
+		
+		String value = values.get(index);			
+		return (StringUtils.isNullOrEmpty(value))? Constants.MISSING_DATA_STRING: value;
 	}
 	
 	/**
@@ -61,38 +79,116 @@ final Map<String, String> map;
 	 * @param value: new field value will append/replace to sample column
 	 */
 	public void setField(String key, String value){
-		if (StringUtils.isNullOrEmpty(key))  
-			throw new IllegalArgumentException("null or empty key passed to getField");
-		
-		map.put(key, (StringUtils.isNullOrEmpty(value)) ? Constants.MISSING_DATA_STRING : value);
+		setField(-1,   key,   value);
 	}	
 	
+	/**
+	 * @param index: add key value to index position, -1 means regardless position
+	 * @param key: add/replace this string to format column
+	 * @param value: add/replace this string to sample column
+	 */
+	public void setField(final int index, String key, String value){
+		if (StringUtils.isNullOrEmpty(key))  
+			throw new IllegalArgumentException("null or empty key passed to getField");
+		 
+		if(keys.size() != values.size())
+			throw new ArrayStoreException("existing format key and value size is not matched!");
+				
+		value = (StringUtils.isNullOrEmpty(value)) ? Constants.MISSING_DATA_STRING : value; 
+		final int order = getKeyIndex( key);
+		if(order < 0 && index < 0){
+			// add new one to end
+			keys.add(key);
+			values.add(value);
+		}else if(order >= 0 && index >= 0){
+			//replace existing one and move to index order
+			keys.remove(order);
+			values.remove(order);
+			keys.add(index, key);
+			values.add(index, value);								
+		}else if(order < 0 && index >= 0){
+			//add new one to index order
+			keys.add(index, key);
+			values.add(index, value);			
+		}else if(order >=0 && index < 0){
+			//replace exist pair with new one
+			keys.set(order, key);
+			values.set(order, value);						
+		}
+		
+	}	
 	/**
 	 * 
 	 * @return Format column String. eg.  GT:GQ:DP:HQ
 	 */
 	public String getFormatColumnString(){
-		return map.keySet().stream().collect(Collectors.joining(Constants.COLON_STRING));
+		return keys.stream().collect(Collectors.joining(Constants.COLON_STRING));
 	}
 	/**
 	 * 
 	 * @return true if sampleColumn is null;  or each value is ".", null or  empty string only
 	 */
 	public boolean isMissingSample(){
-		List<String> uniqueValues = map.values().stream().distinct().collect(Collectors.toList());
-		return (uniqueValues.size() == 1 &&  (uniqueValues.get(0).equals(Constants.MISSING_DATA_STRING)));
+				
+		if(values == null) return true; 
+		
+		boolean flag = true;
+		for( String v : values) {
+			if(!StringUtils.isMissingDtaString(v)){
+				flag = false;
+				break;
+			}
+		}
+		return flag;
 	}
 
 	/**
 	 * @return return sample column String followed Format column pattern: eg.  0|0:48:1:51,51
 	 */
+	
 	public String getSampleColumnString(){
-		if (map == null || map.isEmpty()) return null;
-		return map.values().stream().collect(Collectors.joining(Constants.COLON_STRING));
+//		if (values.isEmpty()) return null;
+//		return values.stream().collect(Collectors.joining(Constants.COLON_STRING));
+		String sample = null;
+		for (String value : values) 
+			if(StringUtils.isNullOrEmpty(sample))
+				sample = value;
+			else		 
+				sample += Constants.COLON + value;		 
+		
+		return (StringUtils.isNullOrEmpty(sample)) ? null: sample;
 	}
 	
 	@Override
 	public String toString(){
 		return getFormatColumnString() + "\t" + getSampleColumnString();
+	}
+	
+	/**
+	 * 
+	 * @return a list of string with two elements: formatColumnString and sampleColumnString
+	 */
+	public List<String> toStringList(){		
+		List<String> list = new ArrayList<String>();		
+		list.add(getFormatColumnString());
+		list.add(getSampleColumnString());
+		return list; 	
+		
+	}
+	
+	/**
+	 * 
+	 * @param key : format column key
+	 * @return the order of exsiting key; return -1 if not exist
+	 */
+	private int getKeyIndex(String key){
+		
+		int index = -1;
+		for(int i = 0; i < keys.size(); i ++) 
+			if(keys.get(i).equals(key)){
+				index = i;
+				break;
+			}
+		return index; 
 	}
 }
