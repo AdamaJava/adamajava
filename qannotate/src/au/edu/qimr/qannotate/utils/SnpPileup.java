971f23f40d408e2875065f6181b305ef7326b472
holmeso <holmeso@users.noreply.github.com>
2018-06-12 05:40:53 +0000

    removed unused vars, make final

--- qannotate/src/au/edu/qimr/qannotate/utils/SnpPileup.java
+++ qannotate/src/au/edu/qimr/qannotate/utils/SnpPileup.java
@@ -1,21 +1,12 @@
 package au.edu.qimr.qannotate.utils;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import htsjdk.samtools.SAMRecord;
-import htsjdk.samtools.SAMTagUtil;
 import org.qcmg.common.model.ChrPosition;
-import org.qcmg.common.util.BaseUtils;
-import org.qcmg.picard.util.SAMUtils;
 
 /**
  * pileup on snp position, retrive the number of discard read, duplicate read, overlapped pairs, forward/backward reads, novel start reads for each Alleles
@@ -23,13 +14,11 @@ import org.qcmg.picard.util.SAMUtils;
  *
  */
 public class SnpPileup {
-	private static final short MD_TAG = SAMTagUtil.getSingleton().MD;
+//	private static final short MD_TAG = SAMTagUtil.getSingleton().MD;
 	private final ChrPosition pos;
 	private int overSame = 0; //number of fragment with overlap reads and contain same base
 	private int overDiff = 0; //number of fragment with overlap reads but different base
-	private int unOver = 0; //number of fragement without overlap
-	private int duplicate = 0; //number of fragement without overlap
-	private int[] atgco = new int[5]; //counts of fragment on pileup base on A T G C and others
+	private final int[] atgco = new int[5]; //counts of fragment on pileup base on A T G C and others
 	int coverage = 0;
 	
 	/**
@@ -38,20 +27,18 @@ public class SnpPileup {
 	 * @param ref: here we can only deal with SNP that is a single char
 	 * @param pool
 	 */
-	public SnpPileup(ChrPosition chrP, List<SAMRecord>  pool){ 
+	public SnpPileup(ChrPosition chrP, List<SAMRecord>  pool){
 		this.pos = chrP;
 				
 		coverage = pool.size();		//total number of reads
 		Map<String,  List<SAMRecord>> pairPool = new HashMap<>();
-		for(SAMRecord re : pool)
-			if(re.getDuplicateReadFlag()) 
-				duplicate ++;
-			else
-				pairPool.computeIfAbsent(re.getReadName(),k->new ArrayList<SAMRecord>()).add(re);
-		
-		for(List<SAMRecord> pair: pairPool.values()){	
+		for(SAMRecord re : pool) {
+			if( ! re.getDuplicateReadFlag()) {
+				pairPool.computeIfAbsent(re.getReadName(),k->new ArrayList<>()).add(re);
+			}
+		}
+		for(List<SAMRecord> pair: pairPool.values()){
 			if( pair.size() == 1){
-				unOver ++;
 				int base = getSnpBaseCode( pair.get(0) );
 				atgco[base] ++;
 			}else if( pair.size() == 2 ){
@@ -86,7 +73,6 @@ public class SnpPileup {
 	
 	public String getAnnotation(){
 		//coverage[coverage, vendor, secondary ,supply, duplicate,errMD]
-//		String str = String.format("%d[%d,%d,%d,%d,%d,%d,%d,%d,%d]",coverage, overSame, overDiff ,unOver, duplicate, atgco[0], atgco[1], atgco[2], atgco[3], atgco[4] );
 		String str = String.format("%d[%d,%d,A%d,C%d,G%d,T%d,O%d]",coverage, overSame, overDiff, atgco[0], atgco[1], atgco[2], atgco[3], atgco[4] );
 
 		return str;
