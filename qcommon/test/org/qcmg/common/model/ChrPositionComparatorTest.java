f40d533c2e0ac173b43f5b358a59042b124b1f48
delocalizer <conrad.leonard@hotmail.com>
2019-02-02 22:31:43 +1000

    Fix for https://github.com/AdamaJava/adamajava/issues/48 Implement the Comparator returned by ChrPositionComparator.getVCFRecordComparator so that it orders all records with a CHROM that appears in the list to be before all records whose CHROM value does not appear in the list

--- qcommon/test/org/qcmg/common/model/ChrPositionComparatorTest.java
+++ qcommon/test/org/qcmg/common/model/ChrPositionComparatorTest.java
@@ -31,4 +31,27 @@ public class ChrPositionComparatorTest {
 		assertEquals(v3, vcfs.get(2));
 		assertEquals(v4, vcfs.get(3));
 	}
+
+	/**
+	 * Test case when reference list of contigs is a subset of contigs in the records to be sorted
+	 */
+	@Test
+	public void vcfComp2() {
+		
+		List<String> contigs = Arrays.asList("chr1");
+		Comparator<VcfRecord> c = ChrPositionComparator.getVcfRecordComparator(contigs);
+		
+		VcfRecord v1 = VcfUtils.createVcfRecord("chr1", 100);
+		VcfRecord v2 = VcfUtils.createVcfRecord("chr1", 101);
+		VcfRecord v3 = VcfUtils.createVcfRecord("chr2", 400);
+		VcfRecord v4 = VcfUtils.createVcfRecord("chr3", 300);
+		
+		List<VcfRecord> vcfs = Arrays.asList(v2, v4, v1, v3);
+		
+		vcfs.sort(c);
+		assertEquals(v1, vcfs.get(0));
+		assertEquals(v2, vcfs.get(1));
+		assertEquals(v3, vcfs.get(2));
+		assertEquals(v4, vcfs.get(3));
+	}
 }
