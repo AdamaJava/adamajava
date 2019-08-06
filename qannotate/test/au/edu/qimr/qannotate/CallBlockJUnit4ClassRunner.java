129ebba202b5d10a3a2ff684f93ce06f0cddff6b
ChristinaXu2017 <xuqinying@hotmail.com>
2019-08-06 12:56:24 +1000

    During our test compilerjunit4.10 is run, which support TemporaryFolder rule. However, our dependency snpEff-4.0e.jar complied with Junit4.4, which disabled our junit4.10 and make TemporaryFolder rule stop working properly. The reason is TemporaryFolder rule requires BlockJUnit4ClassRunner but junit4.4 call JUnit4ClassRunner which is old. To solve the problem, we can either add a customized runner which inherits all feature from BlockJUnit4ClassRunner, or specify the compiler and runtime classpath on the build file

--- qannotate/test/au/edu/qimr/qannotate/CallBlockJUnit4ClassRunner.java
+++ qannotate/test/au/edu/qimr/qannotate/CallBlockJUnit4ClassRunner.java
@@ -4,9 +4,12 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 /**
- * the default junit runner called by gradle is Unit4ClassRunner which won't pick up TemporaryFolder rule.
- * Here we define an customized run which inherits all feature from BlockJUnit4ClassRunner. 
- * Then we call change the runner to BlockJUnit4ClassRunner on each test class: @RunWith(CallBlockJUnit4ClassRunner.class )
+ * During our test compilerjunit4.10 is run, which support TemporaryFolder rule.
+ * However our dependency snpEff-4.0e.jar complied with Junit4.4, which disabled our junit4.10 and make TemporaryFolder rule stop working properly.  
+ * The reason is TemporaryFolder rule requires BlockJUnit4ClassRunner but junit4.4 call JUnit4ClassRunner which is old. 
+ * 
+ * To solve the problem we define an customized runner which inherits all feature from BlockJUnit4ClassRunner. It also added into our test class: "@RunWith(CallBlockJUnit4ClassRunner.class"
+ * Hence the BlockJUnit4ClassRunner will run again for qannotate unit test compiler. 
  * 
  * @author christix
  *
