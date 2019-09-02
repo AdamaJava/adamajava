f1372868e735631c792f4a6947fb545f185dc15c
holmeso <holmeso@users.noreply.github.com>
2019-09-03 08:52:49 +1000

    updated copyright statment - removed UQ

--- qsignature/src/org/qcmg/sig/Compare.java
+++ qsignature/src/org/qcmg/sig/Compare.java
@@ -1,5 +1,4 @@
 /**
- * © Copyright The University of Queensland 2010-2014.
  * © Copyright QIMR Berghofer Medical Research Institute 2014-2016.
  *
  * This code is released under the terms outlined in the included LICENSE file.
