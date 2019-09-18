9c2e9bd87e39d6cc361860a2cefb2f65edee6e07
holmeso <holmeso@users.noreply.github.com>
2019-09-19 08:15:27 +1000

    deprecate BAseReadGroup

--- qsignature/test/org/qcmg/sig/SignatureGeneratorBespokeTest.java
+++ qsignature/test/org/qcmg/sig/SignatureGeneratorBespokeTest.java
@@ -73,31 +73,6 @@ public class SignatureGeneratorBespokeTest {
 	}
 	
 	@Test
-	public void getEncodedDistString() {
-		assertEquals(null, SignatureGeneratorBespoke.getTotalDist(null));
-		assertEquals(null, SignatureGeneratorBespoke.getEncodedDist(new ArrayList<>()));
-		
-		List<BaseReadGroup> list = new ArrayList<>();
-		list.add(new BaseReadGroup('A',""));
-		assertEquals("1-0-0-0", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.add(new BaseReadGroup('A',""));
-		assertEquals("2-0-0-0", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.add(new BaseReadGroup('T',""));
-		assertEquals("2-0-0-1", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.add(new BaseReadGroup('T',""));
-		assertEquals("2-0-0-2", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.clear();
-		list.add(new BaseReadGroup('C',""));
-		assertEquals("0-1-0-0", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.clear();
-		list.add(new BaseReadGroup('G',""));
-		assertEquals("0-0-1-0", SignatureGeneratorBespoke.getEncodedDist(list));
-		list.clear();
-		list.add(new BaseReadGroup('T',""));
-		assertEquals("0-0-0-1", SignatureGeneratorBespoke.getEncodedDist(list));
-	}
-	
-	@Test
 	public void recordWithNoRG() {
 		SAMFileHeader header = SignatureGeneratorTest.getHeader(true, true, true);
 		/*
