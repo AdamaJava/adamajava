0bc7ec0423e47281f4983c19dd7540287e60af12
christix <Christina.Xu@qimrberghofer.edu.au>
2020-07-17 14:02:34 +1000

    merge similar Executor method, call thread.start() inside getErrCode(), rather than call run method inside constructor

--- qcommon/src/org/qcmg/common/commandline/Executor.java
+++ qcommon/src/org/qcmg/common/commandline/Executor.java
@@ -10,50 +10,36 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Executor {
-	private final StreamConsumer outputStreamConsumer;
-	private final StreamConsumer errorStreamConsumer;
-	private final int errCode;
-
+	private final String execCommand; 
+	
+	private  StreamConsumer outputStreamConsumer;
+	private  StreamConsumer errorStreamConsumer;
+	private  int errCode;	
+	boolean alreadyExecuted = false;	
+	
 	public Executor(String arguments, String qualifiedMainClassName) throws IOException, InterruptedException {
 		String classpath = System.getProperty("java.class.path");
 		String javaCommand = "java -classpath " + classpath + " "
 				+ qualifiedMainClassName + " ";
-		String execCommand = javaCommand + arguments;
-//		System.out.println("execCommand: " + execCommand);
-//		System.out.println("classpath: " + classpath);
-		
-		Process process = Runtime.getRuntime().exec(execCommand);
-		outputStreamConsumer = new StreamConsumer(process.getInputStream());
-		errorStreamConsumer = new StreamConsumer(process.getErrorStream());
-		outputStreamConsumer.run();
-		errorStreamConsumer.run();
-		errCode = process.waitFor();
+		this.execCommand = javaCommand + arguments;
 	}
 	
 	public Executor(String [] arguments, String qualifiedMainClassName) throws Exception {
 		String classpath = System.getProperty("java.class.path");
 		String javaCommand = "java -classpath " + classpath + " "
 		+ qualifiedMainClassName + " ";
-		String execCommand = javaCommand + Arrays.toString(arguments);
-		Process process = Runtime.getRuntime().exec(execCommand);
-		outputStreamConsumer = new StreamConsumer(process.getInputStream());
-		errorStreamConsumer = new StreamConsumer(process.getErrorStream());
-		outputStreamConsumer.run();
-		errorStreamConsumer.run();
-		errCode = process.waitFor();
+		this.execCommand = javaCommand + Arrays.toString(arguments);
 	}
 
 	public Executor(String jvmArgs, String arguments, String qualifiedMainClassName) throws Exception {
 		String classpath = System.getProperty("java.class.path");
 		String javaCommand = "java -classpath " + classpath + " " + jvmArgs + " "
 				+ qualifiedMainClassName + " ";
-		String execCommand = javaCommand + arguments;
-		Process process = Runtime.getRuntime().exec(execCommand);
-		outputStreamConsumer = new StreamConsumer(process.getInputStream());
-		errorStreamConsumer = new StreamConsumer(process.getErrorStream());
-		outputStreamConsumer.run();
-		errorStreamConsumer.run();
-		errCode = process.waitFor();
+		this.execCommand = javaCommand + arguments;
+	}
+
+	public Executor(String execCommand) throws Exception {
+		this.execCommand = execCommand;
 	}
 
 	public StreamConsumer getOutputStreamConsumer() {
@@ -64,7 +50,27 @@ public class Executor {
 		return errorStreamConsumer;
 	}
 
-	public int getErrCode() {
-		return errCode;
+	public int getErrCode() throws IOException, InterruptedException {
+		if( alreadyExecuted) { return errCode; }
+		
+		Process process = Runtime.getRuntime().exec(execCommand);
+		outputStreamConsumer = new StreamConsumer(process.getInputStream());
+		errorStreamConsumer = new StreamConsumer(process.getErrorStream());
+		outputStreamConsumer.start();;
+		errorStreamConsumer.start();;
+		errCode = process.waitFor();		
+		alreadyExecuted =  true;
+		
+		return errCode;		
 	}
+
+
+//	public boolean isSuccessful() {
+//		return 0 == errCode;
+//	}
+//
+//
+//	public boolean isFailure() {
+//		return 0 != errCode;
+//	}
 }
