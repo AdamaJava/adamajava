93429a45fb779aa6ed3662813481d4ad74921048
Christina Xu <Christina.Xu@qimrberghofer.edu.au>
2020-08-20 19:35:26 +1000

    stylecheck fix

--- q3indel/src/au/edu/qimr/indel/Main.java
+++ q3indel/src/au/edu/qimr/indel/Main.java
@@ -1,7 +1,7 @@
 package au.edu.qimr.indel;
 
-import org.qcmg.common.log.QLogger;
 import au.edu.qimr.indel.pileup.IndelMT;
+import org.qcmg.common.log.QLogger;
 import org.qcmg.common.util.LoadReferencedClasses;
 
 public class Main {
