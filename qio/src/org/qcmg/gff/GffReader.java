ab974a4e4a28581e683c202a85655daa07017299
christix <christina.xu@qimrberghofer.edu.au>
2020-10-28 15:52:43 +1000

    create GffReader based on the recordReader class

--- qio/src/org/qcmg/gff/GffReader.java
+++ qio/src/org/qcmg/gff/GffReader.java
@@ -1,13 +1,12 @@
 package org.qcmg.gff;
 
 import java.io.File;
-import java.io.IOException;
 import org.qcmg.record.RecordReader;
 
-public class GffReader extends RecordReader<GffRecord> {
+public final class GffReader extends RecordReader<GffRecord> {
 	public static final String TAB_DELIMITER = "\t";
 	
-	public GffReader(File file) throws IOException {
+	public GffReader(File file) throws Exception {
 		super(file);
 	}
 
