74f691493caec247293bd6c1f52c2f58838bf087
Christina Xu <christina.xu@qimrberghofer.edu.au>
2020-11-30 12:39:37 +1000

    mv SimpleSerializerTest to qio/FastaRecordTest

--- qio/test/org/qcmg/qio/fasta/FastaRecordTest.java
+++ qio/test/org/qcmg/qio/fasta/FastaRecordTest.java
@@ -1,35 +1,25 @@
-package org.qcmg.simple;
+package org.qcmg.qio.fasta;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.qcmg.qmule.record.SimpleRecord;
-import org.qcmg.simple.SimpleSerializer;
+import org.qcmg.qio.fasta.FastaRecord;
 
-
-public class SimpleSerializerTest {
+public class FastaRecordTest {
 
 	@Test
 	public void testParseIDInvalid() throws Exception {
 		// test empty string
 		try {
-			SimpleSerializer.parseID("");
+			(new FastaRecord()).setId("");
 			Assert.fail("Should have thrown an Exception");
 		} catch (Exception e) {
 			Assert.assertEquals("Bad id format: ", e.getMessage());
 		}
-		
-		// test null
-//		try {
-//			SimpleSerializer.parseID(null);
-//			Assert.fail("Should have thrown an Exception");
-//		} catch (Exception e) {
-//			Assert.assertEquals("Bad id format", e.getMessage());
-//		}
-		
+				
 		// string containing anything other than '>'
 		String testString = "this is an id $%^&*()";
 		try {
-			SimpleSerializer.parseID(testString);
+			(new FastaRecord()).setId(testString);
 			Assert.fail("Should have thrown an Exception");
 		} catch (Exception e) {
 			Assert.assertEquals("Bad id format: " + testString, e.getMessage());
@@ -38,7 +28,8 @@ public class SimpleSerializerTest {
 		// string containing '>' but not at the start..
 		testString = "this is an > id";
 		try {
-			SimpleSerializer.parseID(testString);
+			
+			(new FastaRecord()).setId(testString);
 			Assert.fail("Should have thrown an Exception");
 		} catch (Exception e) {
 			Assert.assertEquals("Bad id format: " + testString, e.getMessage());
@@ -47,21 +38,27 @@ public class SimpleSerializerTest {
 	
 	@Test
 	public void testParseID() throws Exception {
+		FastaRecord frecord = new FastaRecord();		
 		String returnedID;
-		
-		returnedID = SimpleSerializer.parseID(">123");
+				
+		frecord.setId(">123");
+		returnedID = frecord.getId(); 
 		Assert.assertNotNull(returnedID);
 		Assert.assertEquals(">123", returnedID);
 		
-		returnedID = SimpleSerializer.parseID(">");
+		
+		frecord.setId(">");
+		returnedID = frecord.getId(); 
 		Assert.assertNotNull(returnedID);
 		Assert.assertEquals(">", returnedID);
 		
-		returnedID = SimpleSerializer.parseID(">123_456_789");
+		frecord.setId(">123_456_789");
+		returnedID = frecord.getId();
 		Assert.assertNotNull(returnedID);
 		Assert.assertEquals(">123_456_789", returnedID);
 		
-		returnedID = SimpleSerializer.parseID(">>>");
+		frecord.setId(">>>");
+		returnedID = frecord.getId();
 		Assert.assertNotNull(returnedID);
 		Assert.assertEquals(">>>", returnedID);
 		
@@ -70,29 +67,10 @@ public class SimpleSerializerTest {
 	@Test
 	public void testParseSequenceInvalid() throws Exception {
 		// test null
-//		try {
-//			SimpleSerializer.parseSequence(null);
-//			Assert.fail("Should have thrown an exception");
-//		} catch (Exception e) {
-//			Assert.assertEquals("Bad sequence format", e.getMessage());
-//		}
-//		// test empty string
-//		try {
-//			SimpleSerializer.parseSequence("");
-//			Assert.fail("Should have thrown an Exception");
-//		} catch (Exception e) {
-//			Assert.assertEquals("Bad sequence format", e.getMessage());
-//		}
-//		try {
-//			SimpleSerializer.parseSequence("								");
-//			Assert.fail("Should have thrown an Exception");
-//		} catch (Exception e) {
-//			Assert.assertEquals("Bad sequence format", e.getMessage());
-//		}
 		// test id string
 		String testString = ">123";
 		try {
-			SimpleSerializer.parseSequence(testString);
+			(new FastaRecord()).setData(testString);
 			Assert.fail("Should have thrown an Exception");
 		} catch (Exception e) {
 			Assert.assertEquals("Bad sequence format: " + testString, e.getMessage());
@@ -101,22 +79,24 @@ public class SimpleSerializerTest {
 	
 	@Test
 	public void testParseSequence() throws Exception {
+		FastaRecord frecord = new FastaRecord();	
 		
-		String returnedSequence = SimpleSerializer.parseSequence("G0103200103201032001033001032001032001032001032001");
+		frecord.setData("G0103200103201032001033001032001032001032001032001");
+		String returnedSequence = frecord.getData(); 
 		Assert.assertNotNull(returnedSequence);
-		Assert.assertEquals("G0103200103201032001033001032001032001032001032001", returnedSequence);
+		Assert.assertEquals("G0103200103201032001033001032001032001032001032001", returnedSequence);	
 		
-		returnedSequence = SimpleSerializer.parseSequence("31,30,32,26,26,26,23,24,29,31,31,23,25,18,14,20,18,11,27,22,18,23,2,18,29,20,25,11,19,18");
+		frecord.setData("31,30,32,26,26,26,23,24,29,31,31,23,25,18,14,20,18,11,27,22,18,23,2,18,29,20,25,11,19,18");
+		returnedSequence = frecord.getData(); 
 		Assert.assertNotNull(returnedSequence);
 		Assert.assertEquals("31,30,32,26,26,26,23,24,29,31,31,23,25,18,14,20,18,11,27,22,18,23,2,18,29,20,25,11,19,18", returnedSequence);
 	}
 	
 	@Test
 	public void testParseRecords() throws Exception {
-		SimpleRecord record;
+		FastaRecord record = new FastaRecord(">123", "G0103200103201032001033001032001032001032001032001");
 		
 		// real record
-		record = SimpleSerializer.parseRecord(">123", "G0103200103201032001033001032001032001032001032001");
 		Assert.assertNotNull(record);
 		Assert.assertEquals(">123", record.getId());
 		Assert.assertEquals("G0103200103201032001033001032001032001032001032001", record.getData());
