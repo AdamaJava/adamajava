12a6c138f66deee9e05c087ab7425e4f7c0120cc
christix <christina.xu@qimrberghofer.edu.au>
2021-01-29 22:49:30 +1000

    tidy up qmule which used QSnpRecord

--- qmule/test/org/qcmg/qmule/snppicker/GatkUniqueSnpsTest.java
+++ qmule/test/org/qcmg/qmule/snppicker/GatkUniqueSnpsTest.java
@@ -11,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.qcmg.common.util.SnpUtils;
 import org.qcmg.common.vcf.header.VcfHeaderUtils;
-import org.qcmg.pileup.QSnpRecord;
+import org.qcmg.common.model.QSnpRecord;
 
 public class GatkUniqueSnpsTest {
 	
