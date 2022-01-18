92123356a18f2474965662feaf767dfdb1e2d030
Christina Xu <christina.xu@qimrberghofer.edu.au>
2022-01-19 00:34:48 +1000

    add unit test for output-format

--- qcoverage/test/org/qcmg/coverage/CoverageTest.java
+++ qcoverage/test/org/qcmg/coverage/CoverageTest.java
@@ -16,6 +16,7 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 import org.qcmg.common.commandline.Executor;
 import org.qcmg.qio.gff3.Gff3Record;
 import org.qcmg.qio.record.RecordWriter;
@@ -23,56 +24,53 @@ import org.qcmg.qio.record.RecordWriter;
 import htsjdk.samtools.SAMFileHeader.SortOrder;
 
 public class CoverageTest {
-	static String inputBam;
-	static String inputBai;
-	static String inputGff3;
-	static Path tmpDir;
+	private String inputBam;
+	private String inputBai;
+	private String inputGff3;
 	private String fname;
-	static Gff3Record record;
+	private String log;
 
 	
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+	 @Rule
+	 public TemporaryFolder testFolder = new TemporaryFolder();
 	
 	
-	@BeforeClass
-	 public static void setup() throws IOException {
-		 tmpDir = Files.createTempDirectory(null);
-		 inputBam = Files.createTempFile(tmpDir, null, ".bam").toString();
+	@Before
+	 public void setup() throws IOException {
+		 inputBam = testFolder.newFile("coverage.bam").getAbsolutePath();
 		 inputBai = inputBam.replace("bam", "bai");
 		 SequenceCoverageTest.createCoverageBam(inputBam, SequenceCoverageTest.getAACSAMRecords(SortOrder.coordinate), SequenceCoverageTest.createSamHeaderObject(SortOrder.coordinate));
 		 
-		record = new Gff3Record();
-		record.setSeqId("chr1");
-		record.setType("exon");
-		record.setScore(".");
-		record.setSource(".");
-		record.setStrand("+");
+			Gff3Record record = new Gff3Record();
+			record.setSeqId("chr1");
+			record.setType("exon");
+			record.setStart(100);
+			record.setEnd(102);
+			record.setScore(".");
+			record.setSource(".");
+			record.setStrand("+");
 		
 		 
-		File file = new File(tmpDir + "/test.gff3");
+		File file = testFolder.newFile("test.gff3");  
 		try (RecordWriter<Gff3Record> writer = new RecordWriter<>(file)) {
 			writer.add(record);
 		}
 		inputGff3 = file.getAbsolutePath();
-	 }
-	 
-	 @AfterClass
-	 public static void tearDown() {
-		tmpDir.toFile().delete();
+		log = inputBam.replace("bam", "log");
+		fname = testFolder.getRoot().getAbsolutePath()+"/output";
 	 }
 
-	@Before
-	public final void before() {
-		Random rand = new Random();
-		int int_random = rand.nextInt(1000); 
-	 	fname = tmpDir.toString() + "/output" + int_random;
-	}
+ 
 	
 	@Test
 	public final void DefaultTest() throws Exception {
+		
+ 	 	
+		String cmd = "--log " + log + " --type phys  --input-gff3 " + inputGff3 + " --input-bam " + inputBam + 
+				" --input-bai " + inputBai +  " --output " +fname ;
+
 		//default value txt output only
-		Executor exec = execute(getCmd(""));		
+		Executor exec = execute(cmd);		
 		assertTrue(0 == exec.getErrCode());
 				
 		File fOutput = new File(fname + ".txt");
@@ -84,8 +82,11 @@ public class CoverageTest {
 	
 	@Test
 	public final void XmlTest() throws Exception {
+		String cmd = "--log " + log + " --type phys  --input-gff3 " + inputGff3 + " --input-bam " + inputBam + 
+				" --input-bai " + inputBai +  " --output " +fname  + " --output-format xml";
+
 		//xml output only
-		Executor exec = execute(getCmd(" --output-format xml"));		
+		Executor exec = execute(cmd);		
 		assertTrue(0 == exec.getErrCode());
 		
 		File fOutput = new File(fname + ".txt");
@@ -107,7 +108,7 @@ public class CoverageTest {
 		//debug
 		System.out.println("getCmd fname: " + fname);
 		
-		return "--log " + tmpDir + "/logfile --type phys  --input-gff3 " + inputGff3 + " --input-bam " + inputBam +  " --output " +fname + format;
+		return "--log " + log + " --type phys  --input-gff3 " + inputGff3 + " --input-bam " + inputBam +  " --output " +fname + format;
 	}
 
 	private Executor execute(final String command) throws Exception {
