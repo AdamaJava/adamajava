efb4770d2d4adecb6a3aede0cd5f95150e89ac1e
holmeso <holmeso@users.noreply.github.com>
2022-03-31 11:46:42 +1000

    fix(qsv): loading the positions from index file using incorrect offset
    The start position for each contig was taken from index file rather than the previous contig end
    position + 1 (which is how the positons in the tiled aligner file are derived).

--- q3tiledaligner/src/au/edu/qimr/tiledaligner/PositionChrPositionMap.java
+++ q3tiledaligner/src/au/edu/qimr/tiledaligner/PositionChrPositionMap.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.qcmg.common.log.QLogger;
