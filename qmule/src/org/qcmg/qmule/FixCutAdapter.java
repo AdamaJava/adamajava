ae2129f64d016d99aa9e542a2a71fa1b3eb15dcf
Christina Xu <christina.xu@qimrberghofer.edu.au>
2022-07-09 00:12:45 +1000

    add unit test

--- qmule/src/org/qcmg/qmule/FixCutAdapter.java
+++ qmule/src/org/qcmg/qmule/FixCutAdapter.java
@@ -16,17 +16,20 @@ import htsjdk.samtools.fastq.FastqWriterFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class FixCutAdapter extends CommandLineProgram {
-    static final String USAGE = "A Fastq file may contains empty seqence and base quality after cutAdapter,  Here A base N is set to empty sequence with 0 base quality value";
+    static final String USAGE = "A Fastq file may contains empty seqence after cutAdapter,  Here A base N is set to empty sequence with 0 base quality value";
     
     @Argument(shortName = StandardOptionDefinitions.INPUT_SHORT_NAME, doc = "Input Fastq file to be fixed.")
     public File INPUT;
 
     @Argument(shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc = "Where to write fixed fastq file.")
     public File OUTPUT;
+       
     
     private int line=1;
+    private int fixes = 0;
 
     public static void main(final String[] argv) {
     	
@@ -43,7 +46,6 @@ public class FixCutAdapter extends CommandLineProgram {
         try( BufferedReader reader = IOUtil.openFileForBufferedReading(INPUT);
         		FastqWriter writer =  (new FastqWriterFactory()).newWriter(OUTPUT);){
         	
-        	 
         	do {
         		
             	final String seqHeader = reader.readLine(); // read header             
@@ -52,9 +54,12 @@ public class FixCutAdapter extends CommandLineProgram {
                 String qualLine = reader.readLine(); // Read quality line
         	     
                 //end of file   
-            	if ( seqHeader == null || seqLine == null || qualHeader == null || qualLine == null ) {
-            		break ;          	
-            	}
+//            	if ( seqHeader == null || seqLine == null || qualHeader == null || qualLine == null ) {
+//            		break ;          	
+//            	}
+                
+                if(seqHeader == null) break;
+                
             	
                 if (!seqHeader.startsWith(FastqConstants.SEQUENCE_HEADER)) {
                     throw new SAMException(error("Sequence header must start with " + FastqConstants.SEQUENCE_HEADER + ": " + seqHeader));
@@ -62,20 +67,28 @@ public class FixCutAdapter extends CommandLineProgram {
                 if (!qualHeader.startsWith(FastqConstants.QUALITY_HEADER)) {
                     throw new SAMException(error("Quality header must start with " + FastqConstants.QUALITY_HEADER + ": "+ qualHeader));
                 }
-                if (StringUtil.isBlank(seqLine)) seqLine = "N";
-                if (StringUtil.isBlank(qualLine)) qualLine = "!";
+                if (StringUtil.isBlank(seqLine) || StringUtil.isBlank(qualLine)) { 
+                	seqLine = "N";
+                	qualLine = "!";
+                	fixes ++;
+                } 
+               
 
                 //output updated fastq record
-                FastqRecord frec = new FastqRecord(seqHeader, seqLine, qualHeader, qualLine);
+                final FastqRecord frec = new FastqRecord(seqHeader.substring(1, seqHeader.length()), seqLine,
+                        qualHeader.substring(1, qualHeader.length()), qualLine);
+
                 writer.write(frec); 
         		
                 line += 4 ;
         	} while( true );
         	        	
-        } catch (IOException e) {
+        } catch (IOException | SAMException e) {
 			e.printStackTrace();
 			return 1;
-		}        
+		}   
+        
+        System.err.println("[" + new Date() + "] Total " + (line / 4) + " fastq records are processed, " + fixes + " of them with empty sequence are fixed to single base N");
 		return 0;
 	}
 	
