99e7cec0aedb8a7db0717265f2f85f36846ec99f
Christina Xu <christina.xu@qimrberghofer.edu.au>
2022-10-24 23:16:51 +1000

    new reader will use SeekableStream open file with extension .bam even it is sam file

--- qbamfix/src/org/qcmg/qbamfix/ReheadFinalBAM.java
+++ qbamfix/src/org/qcmg/qbamfix/ReheadFinalBAM.java
@@ -7,6 +7,7 @@
 package org.qcmg.qbamfix;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +17,14 @@ import org.qcmg.picard.SAMOrBAMWriterFactory;
 
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SamReader;
+import htsjdk.samtools.ValidationStringency;
 import htsjdk.samtools.SAMFileWriter;
 import htsjdk.samtools.SAMReadGroupRecord;
 import htsjdk.samtools.SAMRecord;
 
 @Deprecated
-/** 
- * this class is hardly used anywhere. 
+/**
+ * this class is hardly used and seems dated. 
  * @author christix
  *
  */
@@ -33,43 +35,43 @@ public class ReheadFinalBAM {
 	//just for unit test
 	public ReheadFinalBAM(){}
 
-	public ReheadFinalBAM(SAMFileHeader header, NewOptions options, QLogger logger ) throws Exception{
-//		SAMFileHeader header = reader.getFileHeader().clone();				
+	public ReheadFinalBAM(SAMFileHeader header, NewOptions options, QLogger logger ) throws Exception  {
 		//get donor information from CO qlimsmeta line
 		String donor = getDonor(header.getComments());
 		String SMvalue = matchDonor(donor, header.getReadGroups());
 		//created new header if SM value in RG line isn't match the donor
 	    if( SMvalue.length() > 0 ){
-	    	SamReader reader = SAMFileReaderFactory.createSAMFileReader(new File(options.getInputFileName()), null, options.getValidation() );  	    	
-	    	logger.info("According qlimsmeta line SM tag value " + SMvalue + " are replaced to " + donor);
-	    	refinalBAM(reader, donor, new File( options.getOutputFileName()) );
-		    reader.close();					    	
+	    	refinalBAM(options.getInputFileName(), options.getValidation(), donor, new File( options.getOutputFileName()) );	    	 
+	    	
+//	    	SamReader reader = SAMFileReaderFactory.createSAMFileReader(new File(options.getInputFileName()),null, options.getValidation() );  	    	
+//	    	logger.info("According qlimsmeta line SM tag value " + SMvalue + " are replaced to " + donor);
+//	    	refinalBAM(reader, donor, new File( options.getOutputFileName()) );
+//		    reader.close();					    	
 	    }
-	    
 	}
-	
 
-	 void refinalBAM(SamReader reader,String donor, File output) throws Exception{
+	 void refinalBAM(String input, ValidationStringency vs, String donor, File output) throws IOException {
 		
-		//replace header
-		SAMFileHeader header = reader.getFileHeader().clone();		
-		List<SAMReadGroupRecord> rglist = new ArrayList<SAMReadGroupRecord>();
-		
-		for(SAMReadGroupRecord re :  header.getReadGroups()){
-			if(! re.getSample().equalsIgnoreCase(donor)){
-				re.setSample(donor);
+		 try(SamReader reader = SAMFileReaderFactory.createSAMFileReader(new File(input),null, vs);){
+			//replace header
+			SAMFileHeader header = reader.getFileHeader().clone();		
+			List<SAMReadGroupRecord> rglist = new ArrayList<SAMReadGroupRecord>();
+			
+			for(SAMReadGroupRecord re :  header.getReadGroups()){
+				if(! re.getSample().equalsIgnoreCase(donor)){
+					re.setSample(donor);
+				}
+				rglist.add(re);
 			}
-			rglist.add(re);
-		}
-		header.setReadGroups(rglist);
-		
-		//append reads to output 
-		SAMOrBAMWriterFactory factory = new SAMOrBAMWriterFactory(header, true, output,2000000 );
-        SAMFileWriter writer = factory.getWriter();
-    	for( SAMRecord record : reader)
-    		 writer.addAlignment(record);
-    	factory.closeWriter();
-    	reader.close();
+			header.setReadGroups(rglist);
+			
+			//append reads to output 
+			SAMOrBAMWriterFactory factory = new SAMOrBAMWriterFactory(header, true, output,2000000 );
+	        SAMFileWriter writer = factory.getWriter();
+	    	for( SAMRecord record : reader)
+	    		 writer.addAlignment(record);
+	    	factory.closeWriter();
+		 }
  
 	}
 	
@@ -104,7 +106,4 @@ public class ReheadFinalBAM {
     		throw new Exception("can't find related donor information on CO qlimsmeta line: " + donor);
     	 
     }
-    
-	
-	
 }
