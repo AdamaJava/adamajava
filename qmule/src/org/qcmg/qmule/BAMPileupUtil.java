1dec392c7a50a7903e54e1a67888d7351d1dabf2
Oliver Holmes <ollieholmes@gmail.com>
2024-04-29 13:22:29 +1000

    style(qmule): minor changes

--- qmule/src/org/qcmg/qmule/BAMPileupUtil.java
+++ qmule/src/org/qcmg/qmule/BAMPileupUtil.java
@@ -19,54 +19,8 @@ public class BAMPileupUtil {
 	public static int SM_CUTOFF = 14;
 	public static int MD_CUTOFF = 3;
 	public static int CIGAR_CUTOFF = 34;
-	
-	public static int readLengthMatchCounter = 0;
-	public static int posiitonInDeletionCounter = 0;
-	
+
 	private static final QLogger logger = QLoggerFactory.getLogger(BAMPileupUtil.class);
-	
-	
-//	public static void examinePileup(List<SAMRecord> sams, VCFRecord record) {
-////		int normalCoverage = 0;
-//		String pileup = "";
-//		String qualities = "";
-//		for (SAMRecord sam : sams ) {
-//			
-//			if ( eligibleSamRecord(sam)) {
-////				++normalCoverage;
-//				
-//				int offset = getReadPosition(sam, record.getPosition());
-//				
-//				if (offset < 0)  {
-//					logger.info("invalid offset position - position falls within deletion?? position: "+ record.getPosition() + ", alignment start: " + sam.getAlignmentStart() + ", alignment end: " + sam.getAlignmentEnd() + ", read length: " + sam.getReadLength() + " cigar: "+ sam.getCigarString());
-//					continue;
-//				}
-//				
-//				if (offset >= sam.getReadLength()) {
-////					throw new Exception("offset [position: " + record.getPosition() + ", read start pos(unclipped): " + sam.getUnclippedStart() + ", read end pos(unclipped): " + sam.getUnclippedEnd()+ "] is larger than read length!!!: " + sam.format());
-//					// set to last entry in sequence
-////					logger.info("adjusting offset to read length -1");
-////					String read = sam.getReadString();
-////					int refPosition = sam.getReferencePositionAtReadPosition(offset);
-//					logger.info("offset: " + offset + ", position: " + record.getPosition() + ", alignment start: " + sam.getAlignmentStart() + ", unclipped alignment start: " + sam.getUnclippedStart() + ", alignment end: " + sam.getAlignmentEnd());
-//					logger.info( sam.format());
-////					offset = sam.getReadLength() -1;
-////					logger.info("char at adjusted offset: " + read.charAt(offset));
-////					logger.info("md tag: " + sam.getStringAttribute("MD"));
-//					continue;
-//				}
-//				
-//				char c = sam.getReadString().charAt(offset);
-//				pileup += sam.getReadNegativeStrandFlag() ? Character.toLowerCase(c) : c;
-//				qualities += sam.getBaseQualityString().charAt(offset);
-//			}
-//		}
-//		
-//		
-//		if (pileup.length() > 0)
-//			record.setPileup(PileupUtil.getPileupCounts(pileup, qualities));
-//		
-//	}
 	
 	/**
 	 * Determines whether a sam record is eligible by applying some filtering criteria.
@@ -82,14 +36,21 @@ public class BAMPileupUtil {
 		if (null == record) return false;
 		Integer sm = record.getIntegerAttribute("SM");
 		return ! record.getDuplicateReadFlag() 
-			&& (null == sm ? false : sm.intValue() > SM_CUTOFF)
+			&& (null != sm && sm > SM_CUTOFF)
 //			&& tallyMDMismatches(record.getStringAttribute("MD")) < MD_CUTOFF	// 
 			&& ((record.getReadPairedFlag() && record.getSecondOfPairFlag() && record.getProperPairFlag()) 
 					|| tallyCigarMatchMismatches(record.getCigar()) > CIGAR_CUTOFF);
 		
 	}
 	
+	/**
+	 * Calculates the tally of match operations in a cigar string.
+	 *
+	 * @param cigar The Cigar object representing the cigar string.
+	 * @return The tally of match operations in the cigar string.
+	 */
 	public static int tallyCigarMatchMismatches(Cigar cigar) {
+
 		int tally = 0;
 		if (null != cigar) {
 			for (CigarElement element : cigar.getCigarElements()) {
