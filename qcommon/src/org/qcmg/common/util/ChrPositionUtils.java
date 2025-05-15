c0d4fb0d9da0a373f9747199c23930abed9298a7
Oliver Holmes <ollieholmes@gmail.com>
2025-05-15 14:49:01 +1000

    cache string length

--- qcommon/src/org/qcmg/common/util/ChrPositionUtils.java
+++ qcommon/src/org/qcmg/common/util/ChrPositionUtils.java
@@ -86,7 +86,7 @@ public class ChrPositionUtils {
 
     public static boolean isDigits(String str) {
         if (str == null || str.isEmpty()) return false;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0, len = str.length(); i < len; i++) {
             char c = str.charAt(i);
             if (c < '0' || c > '9') return false;
         }
